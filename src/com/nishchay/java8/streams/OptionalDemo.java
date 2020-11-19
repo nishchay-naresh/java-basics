05c4a90ea6f533887b6cdd3b5b984cd0d7a16666
nnares <nishchay.naresh@gmail.com>
2020-11-19 12:44:24 +0530

    +u OptionalDemo.java

--- src/com/nishchay/java8/streams/OptionalDemo.java
+++ src/com/nishchay/java8/streams/OptionalDemo.java
@@ -15,20 +15,32 @@ public class OptionalDemo {
 
 
     private static void optionalDemo() {
-        // Exception in thread "main" java.util.NoSuchElementException: No value present
-        Optional<String> anyElement = Collections.EMPTY_LIST.stream().findAny();
-        System.out.println("Find any - " + anyElement); // Optional.empty
-        System.out.println(Optional.empty().equals(anyElement)); // true
 
         Optional<String> nameGt4 =
                 Stream.of("java", "perl")
                         .filter(e -> e.length() > 4)
                         .findFirst();
-
         System.out.println("nameGt4 - " + nameGt4); // Optional.empty
+//        System.out.println("nameGt4 - " + nameGt4.get());
+// Exception in thread "main" java.util.NoSuchElementException: No value present
+
+        Optional<String> nameGt3 =
+                Stream.of("java", "perl")
+                        .filter(e -> e.startsWith("p"))
+                        .findFirst();
+
+        if(nameGt3.isPresent())
+            System.out.println("nameGt3 - " + nameGt3.get()); // java
+
     }
 
     private static void emptyOptionalDemo() {
+
+        // Exception in thread "main" java.util.NoSuchElementException: No value present
+        Optional<String> anyElement = Collections.EMPTY_LIST.stream().findAny();
+        System.out.println("Find any - " + anyElement); // Optional.empty
+        System.out.println(Optional.empty().equals(anyElement)); // true
+
 //        Optional<String> nullOptional = Optional.of(null); //java.lang.NullPointerException
         Optional<String> emptyOptional = Optional.empty();
         System.out.println(emptyOptional.isPresent());
