208838712b289f7388a1eeb79473a617b39c75b5
nnares <nishchay.naresh@gmail.com>
2020-12-02 01:26:55 +0530

    + MatrixPrint.java

---
+++ src/com/nishchay/ds/array/twod/MatrixPrint.java
@@ -0,0 +1,56 @@
+package com.nishchay.ds.array.twod;
+
+
+/*
+ *	----------original matrix-----------
+ *
+ *	2	5	6
+ *	8	9	3
+ *	1	4	7
+ *
+ *	-----printing in desired order-----
+ *
+ *	6	3	7
+ *	5	9	4
+ *	2	8	1
+ *
+ *	-----
+ *	 [
+ *	 c0,2	c1,2	c2,2
+ *	 c0,1	c1,1	c2,1
+ *	 c0,0	c1,0	c2,0
+ *	 ]
+ *
+ *
+ * */
+
+public class MatrixPrint {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {{2, 5, 6}, {8, 9, 3}, {1, 9, 7}};
+        int noOfRows = 3;
+        int noOfCols = 3;
+
+        // printing
+        System.out.println("----------original matrix-----------");
+        for (int i = 0; i < noOfRows; i++) {
+            System.out.print("\n");
+            for (int j = 0; j < noOfCols; j++) {
+                System.out.print(matrix[i][j] + "\t");
+
+            }
+        }
+
+        System.out.println("----------printing in desired order-----------");
+
+        for (int j = noOfCols - 1; j >= 0; j--) {
+            System.out.print("\n");
+            for (int i = 0; i < noOfRows; i++) {
+                System.out.print(matrix[i][j] + "\t");
+
+            }
+        }
+    }
+
+}
