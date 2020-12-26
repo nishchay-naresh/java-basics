473ba4988db10e636f2a1917c60c8535aaee5f4d
nnares <nishchay.naresh@gmail.com>
2020-12-26 22:59:27 +0530

    + LinkedList.java

---
+++ src/com/nishchay/ds/linklist/LinkedList.java
@@ -0,0 +1,56 @@
+package com.nishchay.ds.linklist;
+// Java program for reversing the linked list
+
+class LinkedList {
+
+    static Node head;
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    /* Function to reverse the linked list */
+    Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return head;
+    }
+
+    // prints content of double linked list
+    void printList(Node head) {
+        for (Node p = head; p != null; p = p.next) {
+            System.out.print(p.data + " ");
+        }
+    }
+
+    // Driver Code
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.head = new Node(10);
+        list.head.next = new Node(20);
+        list.head.next.next = new Node(30);
+        list.head.next.next.next = new Node(40);
+
+        System.out.println("Given Linked list");
+        list.printList(head);
+        head = list.reverse(head);
+        System.out.println("");
+        System.out.println("Reversed linked list ");
+        list.printList(head);
+    }
+}
