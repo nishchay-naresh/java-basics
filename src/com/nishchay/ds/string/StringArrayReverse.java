ad1ef46927dafcaa669b03c4b4e9ccefa563ddf0
nnares <nishchay.naresh@gmail.com>
2021-01-21 08:01:17 +0530

    +u com.nishchay.ds.string

--- src/com/nishchay/ds/string/StringArrayReverse.java
+++ src/com/nishchay/ds/string/StringArrayReverse.java
@@ -1,17 +1,17 @@
 package com.nishchay.ds.string;
 
 /*
-* Reverse a String word wise - reverse the string without reversing its individual words
-*       str = i like this program very much
-*       Output: much very program this like i
-*
-* */
+ * Reverse a String word wise - reverse the string without reversing its individual words
+ *       str = i like this program very much
+ *       Output: much very program this like i
+ *
+ * */
 public class StringArrayReverse {
 
     public static void main(String[] args) {
 
-        String str = "java";
-//        String str = "i like this program very much";
+//        String str = "java";
+        String str = "i like this program very much";
 //      Output: much very program this like i
 
         String revStr = strArrayReverse(str);
@@ -25,23 +25,19 @@ public class StringArrayReverse {
         if (str == null) {
             return null;
         }
-        String strArr[] = str.split(" ");
+        String[] strArr = str.split(" ");
+        int size = strArr.length;
 
-        if (strArr.length < 2) {
+        if (size < 2) {
             return str;
         }
 
         String t;
-        int i, j;
-        i = 0;
-        j = strArr.length - 1;
-        while (i < j) {
+        for (int i = 0, j = size - 1; i < j; i++, j--) {
             // swap ith & jth element
             t = strArr[i];
             strArr[i] = strArr[j];
             strArr[j] = t;
-            i++;
-            j--;
         }
 
         // Convert array of strings into a string in Java 8
---
+++ src/com/nishchay/ds/string/StringPalindrome.java
@@ -0,0 +1,24 @@
+package com.nishchay.ds.string;
+
+public class StringPalindrome {
+
+    public static void main(String[] args) {
+
+        System.out.println("  isPalindrome(\"madam\") - " +  isPalindrome("madam"));
+        System.out.println("  isPalindrome(\"radar\") - " +  isPalindrome("radar"));
+        System.out.println("  isPalindrome(\"satya\") - " +  isPalindrome("satya"));
+        System.out.println("  isPalindrome(\"nitin\") - " +  isPalindrome("nitin"));
+        System.out.println("  isPalindrome(\"nolemonnomelon\") - " +  isPalindrome("nolemonnomelon"));
+    }
+
+    private static boolean isPalindrome(String string) {
+        char[] charArray = string.toCharArray();
+
+        for (int i = 0, j = charArray.length - 1; i < j; i++, j--) {
+            if(charArray[i] != charArray[j]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
