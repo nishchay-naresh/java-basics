f4ed8892a4a2e10b2dddfeb4b73e00e50b9c954e
nnares <nishchay.naresh@gmail.com>
2021-01-22 08:41:36 +0530

    +u BlockExecutionOrder.java

--- src/com/nishchay/core/basics/block/BlockExecutionOrder.java
+++ src/com/nishchay/core/basics/block/BlockExecutionOrder.java
@@ -1,23 +1,43 @@
 package com.nishchay.core.basics.block;
 
+
+/*
+* Static Block - A static block is called once, when the class is loaded and initialized by the JVM.
+* Instance Block - An instance initializer is executed when an instance of the class is constructed, just like a constructor.
+*
+* */
 public class BlockExecutionOrder {
 
+    public BlockExecutionOrder() {
+        System.out.println("3 Constructor execution");
+    }
 
     {
-        System.out.println("2 instance block");
+        System.out.println("2 instance block execution");
     }
 
     static	{
-        System.out.println("1 static block");
-        BlockExecutionOrder ref = new BlockExecutionOrder();
+        System.out.println("1 static block execution");
+        BlockExecutionOrder ref1 =  new BlockExecutionOrder();
     }
 
     public static void main(String[] args) {
-        System.out.println("3 main method");
+        BlockExecutionOrder ref1 =  new BlockExecutionOrder();
         System.gc();
     }
 
     protected void finalize(){
-        System.out.println("4 finalize method");
+        System.out.println("4 finalize method"); // no guarantee of its execution
     }
+
 }
+/*
+ * o/p =>
+ *	1 static block execution
+ *	2 instance block execution
+ *	3 Constructor execution
+ *  2 instance block execution
+ * 3 Constructor execution
+ * 4 finalize method
+ *
+ * */
