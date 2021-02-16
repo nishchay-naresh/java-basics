c43b8151a93713dccda4d7bf0e6fd55cab03e455
nnares <nishchay.naresh@gmail.com>
2021-02-16 22:41:09 +0530

    + com.nishchay.ds.array.sort

---
+++ src/com/nishchay/ds/array/sort/BasicSorting.java
@@ -0,0 +1,62 @@
+package com.nishchay.ds.array.sort;
+
+import java.util.Arrays;
+
+public class BasicSorting {
+
+    public static void main(String[] args) {
+
+        int[] arr = {50, 3, 11, 9, 7, 6, 2};
+
+        System.out.println("original array = " + Arrays.toString(arr));
+        bubbleSort(arr);
+//        selectionSort(arr);
+        System.out.println("sorted array = " + Arrays.toString(arr));
+
+    }
+
+    /*
+     * Finding the largest element with each iteration
+     * Like - the way bubble is coming out of water
+     * */
+    public static void bubbleSort(int[] arr) {
+
+        int size = arr.length;
+        for (int i = 1; i < size; i++) {
+            for (int j = 0; j < size - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swipe arr[j] & arr[j+1]
+                    int t = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = t;
+                }
+            }
+        }
+    }
+
+    /*
+     * - finding the smallest element in the array
+     * - swape with the first element for the array
+     * */
+
+    public static void selectionSort(int[] arr) {
+
+        int size = arr.length;
+        for (int i = 0; i < size - 1; i++) {
+
+            int jmin = i;
+            for (int j = i + 1; j < size; j++)
+                if (arr[j] < arr[jmin])
+                    jmin = j;
+
+            if (jmin != i) {
+                // swipe arr[jmin] & arr[i]
+                int t = arr[jmin];
+                arr[jmin] = arr[i];
+                arr[i] = t;
+            }
+
+        }
+
+    }
+}
