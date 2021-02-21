05e9c085460e71ecd45ce6c06381e0b7e067b1b9
nnares <nishchay.naresh@gmail.com>
2021-02-21 16:34:28 +0530

    +u com.nishchay.java8.basic.OptionalDemo.java

--- src/com/nishchay/java8/basic/OptionalDemo.java
+++ src/com/nishchay/java8/basic/OptionalDemo.java
@@ -1,10 +1,13 @@
 package com.nishchay.java8.basic;
 
+import java.util.Optional;
+
 public class OptionalDemo {
 
     public static void main(String[] args) {
 
         NPEExample();
+        NPESuppressUsingOptional();
 
         optionalDemo();
 
@@ -17,18 +20,45 @@ public class OptionalDemo {
         System.out.println(user.getName());
     }
 
+    private static void NPESuppressUsingOptional() {
+        User user = findUserById(999);
+        Optional<User> userOptional = Optional.ofNullable(user);
+
+        if (userOptional.isPresent()) {
+            System.out.println("Value found - " + userOptional.get());
+        } else {
+            System.out.println("Optional is empty");
+        }
+
+        userOptional.ifPresent(value -> {
+            System.out.println("Value found - " + value);
+        });
+
+    }
+
     private static void optionalDemo() {
 
-//        Optional<User> optional = findUserById(999);
+        Optional<User> userOptional = findUserByIdOptionally(555);
 
 
     }
 
+    private static Optional<User> findUserByIdOptionally(int id) {
+
+        Optional<User> emptyOptional = Optional.empty();
+
+        if (id == 555) {
+            User user = new User(555, "Iron Man");
+            Optional<User> userOptional = Optional.of(user);
+            return userOptional;
+        }
+        return emptyOptional;
+    }
 
     private static User findUserById(int id) {
         User user = null;
         if (id == 999) {
-            user = new User(999, "James Gosling");
+            user = new User(999, "Iron Man");
         }
         return user;
     }
@@ -38,11 +68,11 @@ public class OptionalDemo {
 
 class User {
 
-    int id;
-    String name;
+    private int id;
+    private String name;
 
 
-    public User(int id, String name) {
+    User(int id, String name) {
         this.id = id;
         this.name = name;
     }
