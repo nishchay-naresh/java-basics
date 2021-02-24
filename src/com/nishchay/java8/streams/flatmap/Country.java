66e6b65c589d95e50db05611ab0699308f5aa40a
nnares <nishchay.naresh@gmail.com>
2021-02-24 12:11:56 +0530

    + streams.flatmap.Country.java

---
+++ src/com/nishchay/java8/streams/flatmap/Country.java
@@ -0,0 +1,32 @@
+package com.nishchay.java8.streams.flatmap;
+
+import java.util.Optional;
+
+public class Country {
+
+    private String name;
+
+    public Country(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Optional<String> getNameOptionally() {
+        return Optional.of(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+}
