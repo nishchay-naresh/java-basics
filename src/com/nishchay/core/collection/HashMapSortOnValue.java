693c1b37b32fc939e24dd7ceec41b2c6d4819c76
nnares <nishchay.naresh@gmail.com>
2021-03-20 00:21:35 +0530

    +u core.collection.HashMapSortOnValue.java

--- src/com/nishchay/core/collection/HashMapSortOnValue.java
+++ src/com/nishchay/core/collection/HashMapSortOnValue.java
@@ -2,17 +2,31 @@ package com.nishchay.core.collection;
 
 import java.util.*;
 
-/*
-*
-* 1. populate hashMap with values
-* 2. Create a List<Map.Entry> from hashMap
-* 3. sort this List<Map.Entry> based on Map.Entry.getValue()
-* 4. iterate over this sorted List<Map.Entry> and copy its data to a new LinkedHashMap, so that it can preserve the new sorted order
-*
-* */
+
 public class HashMapSortOnValue {
 
     public static void main(String[] args) {
+
+        sortMapByValueEx();
+//        sortIntArray();
+
+    }
+
+
+
+    /*
+    *  input - 1, 3, 5, 7, 2, 1, 5, 3, 3,
+    * output - 3, 3, 3, 5, 5, 1, 1, 7, 2,
+    *
+    * */
+
+    /*
+     * 1. populate hashMap with values
+     * 2. Create a List<Map.Entry> from hashMap
+     * 3. sort this List<Map.Entry> based on Map.Entry.getValue()
+     * 4. iterate over this sorted List<Map.Entry> and copy its data to a new LinkedHashMap, so that it can preserve the new sorted order
+     * */
+    private static void sortMapByValueEx() {
 
         HashMap<String, Integer> marks = new HashMap<>();
 
@@ -26,33 +40,36 @@ public class HashMapSortOnValue {
 
         System.out.println("originalMap = " + marks);
 
-        Map<String, Integer> sortedMarks = sortByValue(marks);
+        Map<String, Integer> sortedMarks = sortMapByValue(marks);
 
         System.out.println("sortedMap = " + sortedMarks);
 
     }
 
+
     // function to sort hashMap by values
-    public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hashMap)
-    {
+    private static HashMap<String, Integer> sortMapByValue(HashMap<String, Integer> hashMap) {
         // Create a list from elements of HashMap
-        List<Map.Entry<String, Integer> > entryList =  new ArrayList<>(hashMap.entrySet());
+        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(hashMap.entrySet());
 
         // Sort the list - based on hashMap value
-//        entryList.sort(Comparator.comparing(HashMap.Entry::getValue));
-        entryList.sort(Map.Entry.comparingByValue());
+//        Comparator<Map.Entry<String, Integer>> valueComparator =  Comparator.comparing(HashMap.Entry::getValue);
+        Comparator<Map.Entry<String, Integer>> valueComparator = Map.Entry.comparingByValue();
+        entryList.sort(valueComparator);
 
         // Sort the list - based on hashMap key
 //        entryList.sort(Comparator.comparing(HashMap.Entry::getKey));
 //        entryList.sort(Map.Entry.comparingByKey());
 
 
-        // put data from sorted list to hashMap
-        HashMap<String, Integer> temp = new LinkedHashMap<>();
+        // put data from sorted list to linkedHashMap
+        HashMap<String, Integer> sortedMap = new LinkedHashMap<>();
         for (Map.Entry<String, Integer> currEntry : entryList) {
-            temp.put(currEntry.getKey(), currEntry.getValue());
+            sortedMap.put(currEntry.getKey(), currEntry.getValue());
         }
-        return temp;
+        return sortedMap;
 
     }
+
+
 }
