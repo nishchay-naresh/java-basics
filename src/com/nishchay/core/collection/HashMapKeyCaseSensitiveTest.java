9232c1aa2e534d0747e0eea4231a5de807d97ec0
nnares <nishchay.naresh@gmail.com>
2021-03-24 18:30:37 +0530

    +u core.collection.HashMapKeyCaseSensitiveTest.java

--- src/com/nishchay/core/collection/HashMapKeyCaseSensitiveTest.java
+++ src/com/nishchay/core/collection/HashMapKeyCaseSensitiveTest.java
@@ -19,9 +19,16 @@ public class HashMapKeyCaseSensitiveTest {
         System.out.println("HTTP Requester from hashMap - " + hashMap.get(key.toLowerCase()));
 
         //Soln -2
-        TreeMap<String, String> treeMap = new TreeMap<>((o1, o2) -> o1.compareToIgnoreCase(o2));
+//        TreeMap<String, String> treeMap = new TreeMap<>((o1, o2) -> o1.compareToIgnoreCase(o2));
+        TreeMap<String, String> treeMap = new TreeMap<>(String::compareToIgnoreCase);
         treeMap.putAll(hashMap);
 
+        // Since we have pass the comparator to treeMap saying compare String by ignoring the case
+        // so get will work on the same principle of ignoring the case
+        System.out.println("treeMap = " + treeMap);
+
         System.out.println("HTTP Requester from treeMap - " + treeMap.get(key));
+        System.out.println("HTTP Requester from treeMap - " + treeMap.get("SERVER1"));
+        System.out.println("HTTP Requester from treeMap - " + treeMap.get("serveR1"));
     }
 }
