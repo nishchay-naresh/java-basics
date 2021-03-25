87388949f628ef27fdc47fcf352609eca02b332f
nnares <nishchay.naresh@gmail.com>
2021-03-25 21:38:56 +0530

    + java8.basic.OptionalDemo1.java

---
+++ src/com/nishchay/java8/basic/OptionalDemo1.java
@@ -0,0 +1,45 @@
+package com.nishchay.java8.basic;
+
+import java.util.Optional;
+
+public class OptionalDemo1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("validateSolicitedFlag(\"F\") - " + validateSolicitedFlag("F"));
+        System.out.println("validateSolicitedFlag(\"R\") - " + validateSolicitedFlag("R"));
+        System.out.println("validateSolicitedFlag(\"Y\") - " + validateSolicitedFlag("Y"));
+        System.out.println("validateSolicitedFlag(\"A\") - " + validateSolicitedFlag("A"));
+        System.out.println("validateSolicitedFlag(\"Z\") - " + validateSolicitedFlag("Z"));
+        System.out.println("validateSolicitedFlag(\"N\") - " + validateSolicitedFlag("N"));
+
+        System.out.println("-------------------------------------");
+
+        System.out.println("validateSolicitedFlagOptionally(\"F\") - " + validateSolicitedFlagOptionally("F"));
+        System.out.println("validateSolicitedFlagOptionally(\"R\") - " + validateSolicitedFlagOptionally("R"));
+        System.out.println("validateSolicitedFlagOptionally(\"Y\") - " + validateSolicitedFlagOptionally("Y"));
+        System.out.println("validateSolicitedFlagOptionally(\"A\") - " + validateSolicitedFlagOptionally("A"));
+        System.out.println("validateSolicitedFlagOptionally(\"Z\") - " + validateSolicitedFlagOptionally("Z"));
+        System.out.println("validateSolicitedFlagOptionally(\"N\") - " + validateSolicitedFlagOptionally("N"));
+
+    }
+
+    private static String validateSolicitedFlag(String flag){
+        if("F".equals(flag) || "R".equals(flag) || "Y".equals(flag)){
+            return "Y";
+        }else if("N".equals(flag)){
+            return "N";
+        }
+        return null;
+    }
+
+    private static String validateSolicitedFlagOptionally(String flag){
+        if(Optional.ofNullable(flag).filter(value -> value.matches("F|R|Y")).isPresent()){
+            return "Y";
+        }else if(Optional.ofNullable(flag).filter(value -> value.matches("N")).isPresent()){
+            return "N";
+        }
+        return null;
+    }
+
+}
