36574104a26912b43caf673fea24b0bfcc7f0a62
nnares <nishchay.naresh@gmail.com>
2021-03-31 11:52:39 +0530

    +u ds.string.freq.StringFrequency.java

--- src/com/nishchay/ds/string/freq/StringFrequency.java
+++ src/com/nishchay/ds/string/freq/StringFrequency.java
@@ -1,31 +1,33 @@
 package com.nishchay.ds.string.freq;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class StringFrequency {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		String[] strArray = "Nothing is as easy as it looks but it looks easy".split(" ");
+        String input = "Nothing is as easy as it looks but it looks easy";
+        String[] strArray = input.split(" ");
 
-		Map<String, Integer> feqMap = getFrequencyMap(strArray);
-		System.out.println(feqMap);
+        System.out.println("input = " + input);
 
-		// HashMap - {but=1, as=2, looks=2, is=1, it=2, easy=2, Nothing=1}
-		// LinkedHashMap - {Nothing=1, is=1, as=2, easy=2, it=2, looks=2, but=1}
-	}
+        Map<String, Integer> freqMap = StringFrequencyUtility.getFrequencyMap(strArray);
+        System.out.println("Map<String, Integer> feqMap = " + freqMap);
 
+        Map<String, Long> freqMap1 = StringFrequencyUtility.getFrequencyMapStream(strArray);
+        System.out.println("Map<String, Long> freqMap1  =  " + freqMap1);
 
-	public static Map<String, Integer> getFrequencyMap(String[] strArray) {
+        Map<String, Long> orderedFreqMap = StringFrequencyUtility.getOrderedFrequencyMapStream(strArray);
+        System.out.println("orderedFreqMap = \t" + orderedFreqMap);
 
-		Map<String, Integer> feqMap = new HashMap<>();
-		Integer freq;
-		for (String currStr : strArray) {
-			freq = feqMap.get(currStr);
-			freq = freq == null ? 1 : ++freq;
-			feqMap.put(currStr, freq);
-		}
-		return feqMap;
-	}
+    }
+
 }
+
+/*
+ * O/P =>
+ *	input = Nothing is as easy as it looks but it looks easy
+ *	Map<String, Integer> feqMap = {but=1, as=2, looks=2, is=1, it=2, easy=2, Nothing=1}
+ *	Map<String, Long> freqMap1  =  {but=1, looks=2, as=2, is=1, it=2, easy=2, Nothing=1}
+ *	orderedFreqMap = 	{Nothing=1, is=1, as=2, easy=2, it=2, looks=2, but=1}
+ * */
