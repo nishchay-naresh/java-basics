04202397e6718e3fc8b09906b1a958f8e71f3c89
nnares <nishchay.naresh@gmail.com>
2021-04-29 00:17:42 +0530

    +u ds.array.sort.SortArrayOf012.java

--- src/com/nishchay/ds/array/sort/SortArrayOf012.java
+++ src/com/nishchay/ds/array/sort/SortArrayOf012.java
@@ -50,6 +50,77 @@ public class SortArrayOf012 {
         }
     }
 
+    /*
+     *	Complexity Analysis:
+     *	Time Complexity: O(n).
+     *	Only two traversals of the array is needed.
+     *	Space Complexity: O(1).
+     *	As no extra space is required.
+     * */
+    private static void sort012_count_1(int[] nums) {
 
+        // count number of zeros, ones and twos
+        int[] freq = countFreq(nums); // first pass
+
+        // overwrite the array
+        int lastFilledIndex = 0;
+
+        // second pass
+        for (int i = 0; i < 3; i++)  // fill 0, 1, 2
+            lastFilledIndex = fill(nums, i, lastFilledIndex, freq[i]);
+    }
+
+    private static int[] countFreq(int[] nums) {
+        int[] freq = new int[3];
+        for (int x : nums) {
+            if (x == 0) freq[0]++;
+            else if (x == 1) freq[1]++;
+            else freq[2]++;
+        }
+        return freq;
+    }
+
+    // return last filled index
+    private static int fill(int[] nums, int e, int index, int freq) {
+        int i = index;
+        while (i < index + freq) {
+            nums[i] = e;
+            i++;
+        }
+        return i;
+    }
+    /*
+     *  Here keeping 3 pointers (hi, low & mid), initializing them as  lo = mid = 0,   hi = n - 1
+     *  mid is the traversing pointer
+     *  if mid == 0,  swap(lo, mid); lo++; mid++;
+     *  if mid == 1,  do nothing simply mid++;;
+     *  if mid == 2,   swap(mid, hi); hi--;
+     *
+     * */
+    private static void sort012_partition(int[] nums) {
+
+        int lo, hi, mid;
+        lo = mid = 0;
+        hi = nums.length - 1;
+
+        while (mid <= hi) {
+            if (nums[mid] == 0) {
+                swap(nums, lo, mid);
+                lo++; mid++;
+            }else if (nums[mid] == 1) {
+                mid++;
+            }else if (nums[mid] == 2) {
+                swap(nums, mid, hi);
+                hi--;
+            }
+        }
+    }
+
+
+    private static void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
 
 }
