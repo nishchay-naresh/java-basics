2d687bcf26c2640d39042b7bb182fe67e2c45a9e
nnares <nishchay.naresh@gmail.com>
2021-05-15 01:38:45 +0530

    + core.enm.EnumTest.java

---
+++ src/com/nishchay/core/enm/EnumTest.java
@@ -0,0 +1,23 @@
+package com.nishchay.core.enm;
+
+public class EnumTest {
+
+
+    public static void main(String[] args) {
+            Days d1 = Days.SUN;
+        String d1Name = d1.name();
+        Days d2 = Days.valueOf(d1Name);
+
+        String res = d1 == d2?"equal":"unequal";
+        System.out.println("d1 == d2 " + res);
+
+        for(Days d : Days.values()){
+            System.out.println(d.name());
+        }
+    }
+
+}
+
+enum Days {
+    MON, TUE, WED, THU, FRI, SAT, SUN
+}
