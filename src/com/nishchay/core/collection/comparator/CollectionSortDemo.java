5592970332496b1995f46c83a1fb265cb13da170
nnares <nishchay.naresh@gmail.com>
2021-07-23 10:59:44 +0530

    +u core.collection.comparator.CollectionSortDemo.java

--- src/com/nishchay/core/collection/comparator/CollectionSortDemo.java
+++ src/com/nishchay/core/collection/comparator/CollectionSortDemo.java
@@ -1,7 +1,6 @@
 package com.nishchay.core.collection.comparator;
 
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -9,7 +8,7 @@ import java.util.List;
 public class CollectionSortDemo {
 
     public static void main(String[] args) {
-        List<Employee> empList  = populateEmployeeList();
+        List<Employee> empList  = Employee.populateEmployeeList();
 
         System.out.println("######## Employee List #######");
         empList.forEach(System.out::println);
@@ -18,7 +17,6 @@ public class CollectionSortDemo {
         System.out.println("######## Default(empNo) ordering #######");
         empList.forEach(System.out::println);
 
-        //
         System.out.println("######## Job wise ordering #######");
         Collections.sort(empList, (e1,e2) -> e1.getJob().compareTo(e2.getJob()));
         empList.forEach(System.out::println);
@@ -26,7 +24,6 @@ public class CollectionSortDemo {
 
         System.out.println("######## Department wise ordering #######");
         Collections.sort(empList, (e1,e2) -> e1.getDeptName().compareTo(e2.getDeptName()));
-
         empList.forEach(System.out::println);
 
 
@@ -38,26 +35,6 @@ public class CollectionSortDemo {
         empList.sort(cityComparator);
         empList.forEach(System.out::println);
 
-    }
-
-
-    public static List<Employee> populateEmployeeList(){
-        List<Employee> empList = new ArrayList<>();
-//        empList.add(new  Employee("ADAMS","RESEARCH","CLERK",7876,"DALLAS"));
-        empList.add(new  Employee("ALLEN","SALES","SALESMAN",7499,"CHICAGO"));
-//        empList.add(new  Employee("BLAKE","SALES","MANAGER",7698,"CHICAGO"));
-        empList.add(new  Employee("CLARK","ACCOUNTING","MANAGER",7782,"NEW YORK"));
-//        empList.add(new  Employee("FORD","RESEARCH","ANALYST",7902,"DALLAS"));
-        empList.add(new  Employee("JAMES","SALES","CLERK",7900,"CHICAGO"));
-//        empList.add(new  Employee("JONES","RESEARCH","MANAGER",7566,"DALLAS"));
-        empList.add(new  Employee("KING","ACCOUNTING","PRESIDENT",7839,"NEW YORK"));
-//        empList.add(new  Employee("MARTIN","SALES","SALESMAN",7654,"CHICAGO"));
-//        empList.add(new  Employee("MILLER","ACCOUNTING","CLERK",7934,"NEW YORK"));
-//        empList.add(new  Employee("SCOTT","RESEARCH","ANALYST",7788,"DALLAS"));
-        empList.add(new  Employee("SMITH","RESEARCH","CLERK",7369,"DALLAS"));
-        empList.add(new  Employee("TURNER","SALES","SALESMAN",7844,"CHICAGO"));
-//        empList.add(new  Employee("WARD","SALES","SALESMAN",7521,"CHICAGO"));
-        return empList;
     }
 
 }
