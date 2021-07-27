ce4c621dff87b24aae106be9b50489996ec86803
nnares <nishchay.naresh@gmail.com>
2021-07-27 11:10:45 +0530

    +u core.collection.comparator

--- src/com/nishchay/core/collection/comparator/TreeSetSortDemo.java
+++ src/com/nishchay/core/collection/comparator/TreeSetSortDemo.java
@@ -26,57 +26,52 @@ public class TreeSetSortDemo {
         System.out.println("stringTreeSet = " + stringTreeSet);
 
     }
-    /*
-     * O/P =>
-     * stringArrayList = [BBB, JJJ, ZZZ, AAA, QQQ, III]
-     * stringTreeSet = [AAA, BBB, III, JJJ, QQQ, ZZZ]
-     * */
 
     private static void treeSetDemoWithComparator() {
 
 
-        Set<Employee> empTSet = new TreeSet<>();
+        List<Employee> employeeList = Employee.populateEmployeeList();
 
-        empTSet.add(new Employee("ALLEN", "SALES", "SALESMAN", 7499, "CHICAGO"));
-        empTSet.add(new Employee("BLAKE", "SALES", "MANAGER", 7698, "CHICAGO"));
-        empTSet.add(new Employee("CLARK", "ACCOUNTING", "MANAGER", 7782, "NEW YORK"));
-        empTSet.add(new Employee("FORD", "RESEARCH", "ANALYST", 7902, "DALLAS"));
-        empTSet.add(new Employee("JAMES", "SALES", "CLERK", 7900, "CHICAGO"));
-        empTSet.add(new Employee("JONES", "RESEARCH", "MANAGER", 7566, "DALLAS"));
-
-        System.out.println("######## Default(empNo) ordering #######");
+        Set<Employee> empTSet = new TreeSet<>(employeeList);
+        System.out.println("                ######## Default(empNo) ordering #######");
         empTSet.forEach(System.out::println);
 
         empTSet = null;
         empTSet = new TreeSet<>(Comparator.comparing(Employee::getEmpName));
-
-        empTSet.add(new Employee("ALLEN", "SALES", "SALESMAN", 7499, "CHICAGO"));
-        empTSet.add(new Employee("BLAKE", "SALES", "MANAGER", 7698, "CHICAGO"));
-        empTSet.add(new Employee("CLARK", "ACCOUNTING", "MANAGER", 7782, "NEW YORK"));
-        empTSet.add(new Employee("FORD", "RESEARCH", "ANALYST", 7902, "DALLAS"));
-        empTSet.add(new Employee("JAMES", "SALES", "CLERK", 7900, "CHICAGO"));
-        empTSet.add(new Employee("JONES", "RESEARCH", "MANAGER", 7566, "DALLAS"));
-        System.out.println("######## City wise ordering #######");
+        empTSet.addAll(employeeList);
+        System.out.println("                ######## Name wise ordering #######");
         empTSet.forEach(System.out::println);
 
+        empTSet = null;
+        empTSet = new TreeSet<>(Comparator.comparing(Employee::getCity));
+        // TreeSet : emp with duplicate city name will be not gets added, only the first added entry will be there
+        empTSet.addAll(employeeList);
+        System.out.println("                ######## City wise ordering #######");
+        empTSet.forEach(System.out::println);
     }
 }
 /*
  * O/P =>
- *	######## Default(empNo) ordering #######
+ *	stringArrayList = [BBB, JJJ, ZZZ, AAA, QQQ, III]
+ *	stringTreeSet = [AAA, BBB, III, JJJ, QQQ, ZZZ]
+ *	                ######## Default(empNo) ordering #######
+ *	Employee{empName='SMITH', deptName='RESEARCH', job='CLERK', empNo=7369, city='DALLAS'}
  *	Employee{empName='ALLEN', deptName='SALES', job='SALESMAN', empNo=7499, city='CHICAGO'}
- *	Employee{empName='JONES', deptName='RESEARCH', job='MANAGER', empNo=7566, city='DALLAS'}
- *	Employee{empName='BLAKE', deptName='SALES', job='MANAGER', empNo=7698, city='CHICAGO'}
+ *	Employee{empName='CLARK', deptName='ACCOUNTING', job='MANAGER', empNo=7782, city='NEW YORK'}
+ *	Employee{empName='KING', deptName='ACCOUNTING', job='PRESIDENT', empNo=7839, city='NEW YORK'}
+ *	Employee{empName='TURNER', deptName='SALES', job='SALESMAN', empNo=7844, city='CHICAGO'}
+ *	Employee{empName='JAMES', deptName='SALES', job='CLERK', empNo=7900, city='CHICAGO'}
+ *	                ######## Name wise ordering #######
+ *	Employee{empName='ALLEN', deptName='SALES', job='SALESMAN', empNo=7499, city='CHICAGO'}
  *	Employee{empName='CLARK', deptName='ACCOUNTING', job='MANAGER', empNo=7782, city='NEW YORK'}
  *	Employee{empName='JAMES', deptName='SALES', job='CLERK', empNo=7900, city='CHICAGO'}
- *	Employee{empName='FORD', deptName='RESEARCH', job='ANALYST', empNo=7902, city='DALLAS'}
- *	######## City wise ordering #######
+ *	Employee{empName='KING', deptName='ACCOUNTING', job='PRESIDENT', empNo=7839, city='NEW YORK'}
+ *	Employee{empName='SMITH', deptName='RESEARCH', job='CLERK', empNo=7369, city='DALLAS'}
+ *	Employee{empName='TURNER', deptName='SALES', job='SALESMAN', empNo=7844, city='CHICAGO'}
+ *	                ######## City wise ordering #######
  *	Employee{empName='ALLEN', deptName='SALES', job='SALESMAN', empNo=7499, city='CHICAGO'}
- *	Employee{empName='BLAKE', deptName='SALES', job='MANAGER', empNo=7698, city='CHICAGO'}
+ *	Employee{empName='SMITH', deptName='RESEARCH', job='CLERK', empNo=7369, city='DALLAS'}
  *	Employee{empName='CLARK', deptName='ACCOUNTING', job='MANAGER', empNo=7782, city='NEW YORK'}
- *	Employee{empName='FORD', deptName='RESEARCH', job='ANALYST', empNo=7902, city='DALLAS'}
- *	Employee{empName='JAMES', deptName='SALES', job='CLERK', empNo=7900, city='CHICAGO'}
- *	Employee{empName='JONES', deptName='RESEARCH', job='MANAGER', empNo=7566, city='DALLAS'}
  *
  * */
 
