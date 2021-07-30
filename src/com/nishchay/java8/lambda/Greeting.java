3584547ed7550c2b107e4bcade2d2ca50355d3e2
nnares <nishchay.naresh@gmail.com>
2021-07-30 11:43:01 +0530

    +u java8.lambda

--- src/com/nishchay/java8/lambda/Greeting.java
+++ src/com/nishchay/java8/lambda/Greeting.java
@@ -7,7 +7,7 @@ public interface Greeting {
 
 //    void anotherMethod();
 /*
-if we are adding any further abstract method to a Functional Interface, errro will come at all the implementation class & lambda code
+if we are adding any further abstract method to a Functional Interface, error will come at all the implementation class & lambda code
 but if we are pitting annotation - @FunctionalInterface, then it will flash the error here only
 error - Multiple non-overriding abstract methods found in interface
 */
