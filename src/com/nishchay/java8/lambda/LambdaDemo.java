501e140e680ab7b91acdd6b733abc55408cb4941
nnares <nishchay.naresh@gmail.com>
2021-07-30 11:43:59 +0530

    +u java8.lambda

--- src/com/nishchay/java8/lambda/LambdaDemo.java
+++ src/com/nishchay/java8/lambda/LambdaDemo.java
@@ -9,17 +9,15 @@ public class LambdaDemo {
         * Two class will be generated -  HelloGreeting.class, LambdaDemo.class
         *
         * */
-
         Greeting helloGreeting =  new HelloGreeting();
         helloGreeting.perform();
+
 
         /*
         * 2. Providing the implementation under an anonymous inner class
         * Two class will be generated -  LambdaDemo.class, LambdaDemo$1.class(main running class)
         *
         * */
-
-
         Greeting innerClassGreeting = new Greeting() {
             @Override
             public void perform() {
@@ -28,17 +26,23 @@ public class LambdaDemo {
         };
         innerClassGreeting.perform();
 
+
         /*
         * 2. Providing the implementation by a lambda expression
         * Only one class will be generated -  Greeting.class
         *
         * */
-
         Greeting lambdaGreeting = () -> System.out.println("lambda expression - greeting");
         lambdaGreeting.perform();
 
     }
 }
 
-
+/*
+* O/P =>
+* implementation class - greeting
+* anonymous inner class - greeting
+* lambda expression - greeting
+*
+* */
 
