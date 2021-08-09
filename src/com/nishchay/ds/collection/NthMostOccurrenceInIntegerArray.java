d12f62bee1d9dd2dd3491db23b97a793f7ba7436
nnares <nishchay.naresh@gmail.com>
2021-08-10 00:31:06 +0530

    +u ds.collection.NthMostOccurrenceInIntegerArray.java

--- src/com/nishchay/ds/collection/NthMostOccurrenceInIntegerArray.java
+++ src/com/nishchay/ds/collection/NthMostOccurrenceInIntegerArray.java
@@ -4,15 +4,22 @@ import java.util.*;
 
 /*
  *	write down a function which returns the no with nth most occurrence in an array of integer
- *	arr[] = {7, 10, 11, 5, 2, 5, 5, 7, 11, 8, 9},
+ *
+ * arr[] = {7, 10, 11, 5, 2, 5, 5, 7, 11, 8, 9},
  *	k = 4
  *	Output: 5 11 7 10
- *	Explanation:
+ *	--------
+ * Explanation:
  *	    freq array : {2->1, 5->3, 7->2, 8->1, 9->1, 10->1, 11->2}
  *	    freq array sorted in descending order of frequency, if freq is same then  sorted in descending order of no
  *	    freq array : {5->3, 11->2, 7->2, 10->1, 9->1, 8->1, 2->1}
  *	    ans - get the first 4 no
+ * ---------
+ *	arr[] = {3, 1, 4, 4, 5, 2, 6, 1},
+ *	k = 2
+ *	Output: 4 1
  *
+ * -------------------------------
  *	  int[] nthMostOccurrence(int[]){
  *
  *	  }
@@ -24,12 +31,22 @@ public class NthMostOccurrenceInIntegerArray {
     public static void main(String[] args) {
 
         int[] arr = {7, 10, 11, 5, 2, 5, 5, 7, 11, 8, 9};
+        int k = 4;
 
-        int[] res = nthMostOccurrence(arr, 4);
-        System.out.println("res = " + Arrays.toString(res)); //[5, 11, 7, 10]
+        int[] res = nthMostOccurrence(arr, k);
+        System.out.println(k + "thMostOccurrence = " + Arrays.toString(res)); //[5, 11, 7, 10]
+
+        arr = new int[]{3, 1, 4, 4, 5, 2, 6, 1};
+        k = 2;
+        res = nthMostOccurrence(arr, 2);
+        System.out.println(k + "thMostOccurrence = " + Arrays.toString(res)); // [4, 1]
 
     }
 
+    /*
+     *	Time Complexity: O(d log d), where d is the count of distinct elements in the array. O(d log d) for sorting
+     *	Auxiliary Space: O(d), where d is the count of distinct elements in the array.
+     * */
     private static int[] nthMostOccurrence(int[] intArray, int k) {
 
         Map<Integer, Integer> freqMap = HashMapSortOnValue.getFrequencyMap(intArray);
@@ -44,6 +61,7 @@ public class NthMostOccurrenceInIntegerArray {
         entryList.sort(valueThenKeyComp);
 
         System.out.println("Sorted List = " + entryList);
+        System.out.println("----------------------------------------------");
 
         int[] result = new int[k];
         for (int i = 0; i < k; i++) {
