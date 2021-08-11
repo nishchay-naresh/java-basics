1c608801ef73bbed77cf0cdcc7861a986d595994
nnares <nishchay.naresh@gmail.com>
2021-08-11 14:02:59 +0530

    + dp.knapsack01.SubsetSumSubsets.java

---
+++ src/com/nishchay/ds/dp/knapsack01/SubsetSumSubsets.java
@@ -0,0 +1,32 @@
+package com.nishchay.ds.dp.knapsack01;
+
+
+/*
+ *	Perfect Sum Problem (Print all subsets with given sum)
+ *	Difficulty Level : Hard
+ *	Last Updated : 02 Mar, 2021
+ *	Given an array of integers and a sum, the task is to print all subsets of the given array with a sum equal to a given sum.
+ *	Examples:
+ *
+ *	Input : arr[] = {2, 3, 5, 6, 8, 10}
+ *	        sum = 10
+ *	Output : 5 2 3
+ *	         2 8
+ *	         10
+ *
+ *	Input : arr[] = {1, 2, 3, 4, 5}
+ *	        sum = 10
+ *	Output : 4 3 2 1
+ *	         5 3 2
+ *	         5 4 1
+ *
+ * https://www.geeksforgeeks.org/perfect-sum-problem-print-subsets-given-sum/
+ * */
+public class SubsetSumSubsets {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
