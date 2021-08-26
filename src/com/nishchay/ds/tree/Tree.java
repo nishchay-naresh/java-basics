0e45380accdc322559ae39699f515c00650d8163
nnares <nishchay.naresh@gmail.com>
2021-08-26 12:50:52 +0530

    + ds.tree.Tree.java

---
+++ src/com/nishchay/ds/tree/Tree.java
@@ -0,0 +1,29 @@
+package com.nishchay.ds.tree;
+
+public class Tree {
+
+    /*
+     * Code to perform BST search
+     *            2
+     *        1       3
+     * */
+
+    /*       Node searchBST(Node root, int key){
+
+            if(root == null){
+                return;
+            }else if(root.data == key){
+                return root;
+            }
+            if(root.left == null && root.left == null){
+                return;
+            }
+            if(root.left != null || root.right != null){
+                searchBST(root.left, key);
+                searchBST(root.right, key);
+            }
+
+        }
+*/
+
+}
