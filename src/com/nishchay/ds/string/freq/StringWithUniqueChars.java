94131463a4431ace305ced8ad3a6a75cc45e3a02
nnares <nishchay.naresh@gmail.com>
2021-10-27 00:39:13 +0530

    +u ds.string.freq.StringWithUniqueChars.java

--- src/com/nishchay/ds/string/freq/StringWithUniqueChars.java
+++ src/com/nishchay/ds/string/freq/StringWithUniqueChars.java
@@ -3,32 +3,44 @@ package com.nishchay.ds.string.freq;
 import java.util.HashSet;
 import java.util.Set;
 
+/*
+ * Problem - test a string , whether it contains only unique chars
+ *
+ * Examples
+ *		if given String is "java" return false
+ * 		if given String is "perl" return true
+ * 		if given String is "apple" return false
+ * 		if given String is "orange" return false
+ * */
 public class StringWithUniqueChars {
 
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println("isUniqueCharsOnlyHashSet(\"\"adiction\"\") - " + isUniqueCharsOnlyHashSet("adiction"));
-		System.out.println("isUniqueCharsOnlyHashSet(\"\"abcdefirtup\"\") - " + isUniqueCharsOnlyHashSet("abcdefirtup"));
+        System.out.println("isUniqueCharsOnlyHashSet(\"java\") - " + isUniqueCharsOnlyHashSet("java"));
+        System.out.println("isUniqueCharsOnlyHashSet(\"perl\") - " + isUniqueCharsOnlyHashSet("perl"));
 
-		System.out.println("isUniqueCharsOnlyStream(\"\"adiction\"\") - " + isUniqueCharsOnlyStream("adiction"));
-		System.out.println("isUniqueCharsOnlyStream(\"\"abcdefirtup\"\") - " + isUniqueCharsOnlyStream("abcdefirtup"));
+        System.out.println("isUniqueCharsOnlyHashSet(\"apple\") - " + isUniqueCharsOnlyHashSet("apple"));
+        System.out.println("isUniqueCharsOnlyHashSet(\"orange\") - " + isUniqueCharsOnlyHashSet("orange"));
 
-	}
+        System.out.println("isUniqueCharsOnlyStream(\"apple\") - " + isUniqueCharsOnlyStream("apple"));
+        System.out.println("isUniqueCharsOnlyStream(\"orange\") - " + isUniqueCharsOnlyStream("orange"));
 
-	public static boolean isUniqueCharsOnlyHashSet(String str){
+    }
 
-		Set<Character> hashSet = new HashSet<>();
+    private static boolean isUniqueCharsOnlyHashSet(String str) {
 
-		for(char c : str.toCharArray()){ //iterate through character array
-			if(!hashSet.add(c))//try to add each char to hashSet
-				return false; //return false if could not add
-		}
-		return true;
-	}
+        Set<Character> hashSet = new HashSet<>();
+
+        for (char c : str.toCharArray()) { //iterate through character array
+            if (!hashSet.add(c))//try to add each char to hashSet
+                return false; //return false if could not add
+        }
+        return true;
+    }
 
 
-	public static boolean isUniqueCharsOnlyStream(String str){
-		return str.length() == str.chars().distinct().count();
-	}
+    private static boolean isUniqueCharsOnlyStream(String str) {
+        return str.length() == str.chars().distinct().count();
+    }
 }
