c2dcd3e66a4f3b775d7ad877c460e3fec8cfb94f
nnares <nishchay.naresh@gmail.com>
2021-11-23 00:19:30 +0530

    +u ds.array.nelement.FindMissingNumberIn1_N.java

--- src/com/nishchay/ds/array/nelement/FindMissingNumberIn1_N.java
+++ src/com/nishchay/ds/array/nelement/FindMissingNumberIn1_N.java
@@ -13,25 +13,55 @@ package com.nishchay.ds.array.nelement;
  *	Explanation: The missing number from 1 to 5 is 4
  *
  * https://www.geeksforgeeks.org/find-repetitive-element-1-n-1/
+ * https://www.youtube.com/watch?v=6KHW7ZQwtCA
  * */
 class FindMissingNumberIn1_N {
 
     /* program to test above function */
     public static void main(String[] args) {
 
-        int[] nums = {8, 2, 4, 6, 3, 7, 1};
-        System.out.println(getMissingNoSum(nums));
+        int[] arr1 =  { 10, 5, 9, 4, 2, 7, 8, 3, 1 };
+        System.out.println(getMissingNoHashMap(arr1)); // 6
 
-        int[] arr =  { 10, 5, 9, 4, 2, 7, 8, 3, 1 };
-        System.out.println(getMissingNoXOR(arr));
+        int[] arr2 = {8, 2, 4, 6, 3, 7, 1};
+        System.out.println(getMissingNoSum(arr2)); // 5
 
+        int[] arr3 =  { 10, 5, 9, 4, 2, 7, 8, 3, 1 }; // 6
+        System.out.println(getMissingNoXOR(arr3));
 
     }
 
     /*
+     * Method 1 - Hashing / freq count
+     * Function to find missing number -  by first having the sum of First N Numbers
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     * */
+    private static int getMissingNoHashMap(int[] nums) {
+
+        int n = nums.length;
+        // n + 1 , bcus n nos are there in array and 1 is missing so n+1
+        // again we are not using index 0, so n + 2
+        int freqArraySize = n + 2;
+        boolean[] freq =  new boolean[freqArraySize];// all cells are initialized with false
+
+        for (int i = 0; i < n; i++) {
+            freq[nums[i]] = true;
+        }
+        int res = -1;
+        for (int i = 1; i < freqArraySize; i++) {
+            if(freq[i] == false)
+                res = i;
+        }
+        return res;
+    }
+
+    /*
+    * Method 2 - Sum formula
     * Function to find missing number -  by first having the sum of First N Numbers
     * Time Complexity: O(n)
     * Space Complexity: O(1)
+    * Problem - since we ae keeping the sum in single int variable, we can run in overflow issue for a bigger array
     * */
     private static int getMissingNoSum(int[] nums) {
         int n = nums.length;
@@ -45,6 +75,7 @@ class FindMissingNumberIn1_N {
 
 
     /*
+     * Method 3 - XOR method
      * Function to find missing number -  by doing XOR of First N Numbers
      * Time Complexity: O(n)
      * Space Complexity: O(1)
