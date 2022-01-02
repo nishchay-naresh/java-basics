0aa882c57efbd1b55954f4499ba0c48c14149cd6
nnares <nishchay.naresh@gmail.com>
2022-01-02 13:40:24 +0530

    + ds.design.median.PQDemo.java

---
+++ src/com/nishchay/ds/design/median/PQDemo.java
@@ -0,0 +1,61 @@
+package com.nishchay.ds.design.median;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class PQDemo {
+
+    public static void main(String[] args) {
+
+        heapPrint();
+        minHeapPrint();
+        maxHeapPrint();
+
+    }
+
+    private static void heapPrint() {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.offer(12);
+        pq.offer(2);
+        pq.offer(8);
+        pq.offer(5);
+        pq.offer(18);
+
+        System.out.println("pq.size() = " + pq.size());
+        Integer val = null;
+        while ((val = pq.poll()) != null) {
+            System.out.print(val + ", ");
+        }
+        System.out.print("\npq.size() = " + pq.size());
+    }
+
+    /*
+     *	Max–heap Property - in a max-heap, the root node always has the largest value.
+     *	Min–heap Property - in a min-heap, the root node always has the smallest value.
+     * */
+    private static void maxHeapPrint() {
+        // Max–heap -  root node always has the largest value, keeps data in descending order
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>();
+        maxHeap.offer(12);
+        maxHeap.offer(2);
+        maxHeap.offer(8);
+        maxHeap.offer(5);
+        maxHeap.offer(18);
+
+        System.out.println("maxHeap = " + maxHeap);
+        System.out.println("maxHeap.peek() = " + maxHeap.peek());
+    }
+
+    private static void minHeapPrint() {
+        // Max–heap -  root node always has the smallest value, keeps data in ascending order
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        minHeap.offer(12);
+        minHeap.offer(2);
+        minHeap.offer(8);
+        minHeap.offer(5);
+        minHeap.offer(18);
+
+        System.out.println("minHeap = " + minHeap);
+        System.out.println("minHeap.peek() = " + minHeap.peek());
+    }
+}
