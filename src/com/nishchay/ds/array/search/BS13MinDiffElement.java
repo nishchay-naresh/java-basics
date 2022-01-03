b6f5c4dc50fb7ebbfeb084ec0dd3c3d0bc00a9e0
nnares <nishchay.naresh@gmail.com>
2022-01-03 19:57:44 +0530

    +u ds.array.search.BS13MinDiffElement.java

--- src/com/nishchay/ds/array/search/BS13MinDiffElement.java
+++ src/com/nishchay/ds/array/search/BS13MinDiffElement.java
@@ -5,7 +5,7 @@ package com.nishchay.ds.array.search;
  *
  *
  *  Given an array of integers sorted in ascending order, and a target value.
- *  find the element in the array that has minimum difference with the target value.
+ *  find the element in the array that has minimum difference with the target value(consider absolute value).
  *
  *	Example 1:
  *		Input: a[] = [2, 5, 10, 12, 15], target = 6
@@ -35,18 +35,69 @@ public class BS13MinDiffElement {
 
     public static void main(String[] args) {
 
-        int[] arr;
+        int[] arr = new int[]{2, 5, 10, 12, 15};
         int target;
 
-        arr = new int[]{2, 5, 10, 12, 15};
-        target = 6;
 
-        System.out.printf("MinimumDifferenceElementWith(%d) = %d%n", target, getMinDifference(arr, target));
+        target = 6;
+        System.out.printf("MinimumDifferenceElementWith(%d) = %d%n", target, getMinDifference(arr, target)); // 5
+
+        target = 5;
+        System.out.printf("MinimumDifferenceElementWith(%d) = %d%n", target, getMinDifference(arr, target)); // 5
+
+        target = 8;
+        System.out.printf("MinimumDifferenceElementWith(%d) = %d%n", target, getMinDifference(arr, target)); // 10
+
+        target = 11;
+        System.out.printf("MinimumDifferenceElementWith(%d) = %d%n", target, getMinDifference(arr, target)); // 10
+
+        target = 20;
+        System.out.printf("MinimumDifferenceElementWith(%d) = %d%n", target, getMinDifference(arr, target)); // 15
 
     }
 
+    /*
+     *  Hint - utilise the BS terminating condition result
+     *
+     *	1. if target itself there in array
+     *       then return arr[mid]
+     *	2. if target is not there in array
+     *       find target's neighbours in array
+     *       then return arr[left] - target <  target - arr[right] ? arr[left] : arr[right]
+     *
+     */
     private static int getMinDifference(int[] arr, int target) {
-        return -1;
+        int n = arr.length;
+
+        if (target < arr[0])
+            return arr[0];
+        if (target > arr[n - 1])
+            return arr[n - 1];
+
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (target == arr[mid]) {
+                return arr[mid];
+            } else if (target < arr[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        /*
+           At the end of the while loop,
+           a[left] is the ceiling of target (Smallest number greater than target), and
+           a[right] is the floor of target (Largest number smaller than target).
+
+           Return the element whose difference with target is smaller
+         */
+        if ((arr[left] - target) < (target - arr[right]))
+            return arr[left];
+        return arr[right];
+
     }
 
 }
