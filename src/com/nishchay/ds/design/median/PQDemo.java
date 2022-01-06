44a7312fcce2bcf49836bdbfaf73cd415350202e
nnares <nishchay.naresh@gmail.com>
2022-01-06 19:39:31 +0530

    +u ds.design.median.PQDemo.java

--- src/com/nishchay/ds/design/median/PQDemo.java
+++ src/com/nishchay/ds/design/median/PQDemo.java
@@ -8,8 +8,11 @@ public class PQDemo {
     public static void main(String[] args) {
 
         heapPrint();
-        minHeapPrint();
+        System.out.println("----------------------------");
         maxHeapPrint();
+        System.out.println("----------------------------");
+        minHeapPrint();
+
 
     }
 
@@ -26,7 +29,7 @@ public class PQDemo {
         while ((val = pq.poll()) != null) {
             System.out.print(val + ", ");
         }
-        System.out.print("\npq.size() = " + pq.size());
+        System.out.println("\npq.size() = " + pq.size());
     }
 
     /*
@@ -34,7 +37,7 @@ public class PQDemo {
      *	Min–heap Property - in a min-heap, the root node always has the smallest value.
      * */
     private static void maxHeapPrint() {
-        // Max–heap -  root node always has the largest value, keeps data in descending order
+        // Max–heap -  root node always has the largest value, keeps data in ascending order
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>();
         maxHeap.offer(12);
         maxHeap.offer(2);
@@ -42,12 +45,12 @@ public class PQDemo {
         maxHeap.offer(5);
         maxHeap.offer(18);
 
-        System.out.println("maxHeap = " + maxHeap);
+        System.out.println("maxHeap = " + maxHeap); // maxHeap = [2, 5, 8, 12, 18]
         System.out.println("maxHeap.peek() = " + maxHeap.peek());
     }
 
     private static void minHeapPrint() {
-        // Max–heap -  root node always has the smallest value, keeps data in ascending order
+        // Min–heap -  root node always has the smallest value, keeps data in descending order
         PriorityQueue<Integer> minHeap = new PriorityQueue<>(Comparator.reverseOrder());
         minHeap.offer(12);
         minHeap.offer(2);
@@ -55,7 +58,7 @@ public class PQDemo {
         minHeap.offer(5);
         minHeap.offer(18);
 
-        System.out.println("minHeap = " + minHeap);
+        System.out.println("minHeap = " + minHeap); // minHeap = [18, 12, 8, 2, 5]
         System.out.println("minHeap.peek() = " + minHeap.peek());
     }
 }
