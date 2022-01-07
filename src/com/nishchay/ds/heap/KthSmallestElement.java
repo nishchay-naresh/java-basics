89cc9c2b5fae01a4cffd735a5e6a18f3591ec0fe
nnares <nishchay.naresh@gmail.com>
2022-01-07 14:31:09 +0530

    + ds.heap.KthSmallestElement.java

---
+++ src/com/nishchay/ds/heap/KthSmallestElement.java
@@ -0,0 +1,34 @@
+package com.nishchay.ds.heap;
+
+/*
+ *============== K’th Smallest/Largest Element in Unsorted Array ====================
+ *
+ *
+ * Given an array and a number k where k is smaller than the size of the array.
+ * We need to find the k’th smallest element in the given array. It is given that all array elements are distinct.
+ *
+ *	Example 1
+ *		Input: arr[] = {7, 10, 4, 3, 20, 15}
+ *		k = 3
+ *		Output: 7
+ *
+ *	Example 2
+ *		Input: arr[] = {7, 10, 4, 3, 20, 15}
+ *		k = 4
+ *		Output: 10
+ *
+ *
+ * https://www.geeksforgeeks.org/kth-smallestlargest-element-unsorted-array/
+ * https://www.techiedelight.com/find-kth-smallest-element-array/
+ * */
+public class KthSmallestElement {
+
+    public static void main(String[] args){
+        int[] arr = new int[] { 12, 3, 5, 7, 19 };
+        int k = 2;
+//        System.out.print("K'th smallest element is " + kthSmallest(arr, k));
+
+
+    }
+
+}
