c70bab9d23fda576ef233f02999a301b50e8882e
nnares <nishchay.naresh@gmail.com>
2022-01-22 20:34:35 +0530

    + ds.array.basic.MoveZeros.java

---
+++ src/com/nishchay/ds/array/basic/MoveZeros.java
@@ -0,0 +1,39 @@
+package com.nishchay.ds.array.basic;
+
+/*
+ *	=========== Move Zeros to the Start/End ===========
+ *
+ * Given an array of random numbers, move all the zero’s of a given array to the start/end of the array.
+ *
+ * For example,
+ *		if the given array is {1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0}
+ *		moving all zeros to end   - {1, 9, 8, 4, 2, 7, 6, 0, 0, 0, 0}
+ *		moving all zeros to start - {0, 0, 0, 0, 1, 9, 8, 4, 2, 7, 6}
+ *
+ *	Example 1:
+ *		Input :  arr[] = {1, 2, 0, 4, 3, 0, 5, 0};
+ *		Output : moving all zeros to end   -  {1, 2, 4, 3, 5, 0, 0, 0};
+ *				moving all zeros to start  -  {0, 0, 0, 1, 2, 4, 3, 5};
+ *
+ *	Example 2:
+ *		Input : arr[]  = {1, 2, 0, 0, 0, 3, 6};
+ *		Output : moving all zeros to end   -  {1, 2, 3, 6, 0, 0, 0};
+ *				moving all zeros to start  -  {0, 0, 0, 1, 2, 3, 6};
+ *
+ *
+ * https://www.educative.io/m/move-zeros-left
+ * https://www.geeksforgeeks.org/move-zeroes-end-array/
+ * */
+public class MoveZeros {
+
+    public static void main(String[] args) {
+
+//        pushEndEx();
+//        pushStartEx();
+
+    }
+
+
+
+}
+
