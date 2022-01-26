55fc62688f60d9aafae9080612feca8f6108cd88
nnares <nishchay.naresh@gmail.com>
2022-01-27 00:06:07 +0530

    +u ds.array.nelement.FindMissingNumbersIn1_NUnSorted.java

--- src/com/nishchay/ds/array/nelement/FindMissingNumbersIn1_NUnSorted.java
+++ src/com/nishchay/ds/array/nelement/FindMissingNumbersIn1_NUnSorted.java
@@ -1,6 +1,13 @@
 package com.nishchay.ds.array.nelement;
 
-import java.util.*;
+import com.nishchay.ds.array.basic.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /*
  *	=========== Find All Numbers Disappeared in an Array ===========
@@ -21,7 +28,7 @@ import java.util.*;
  * https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
  *
  * Find All Numbers Disappeared in an Array:
- * test:[1,5, 3, 4, 7]
+ * test:[1, 5, 3, 4, 7]
  * ans: [2, 6]
  * The solution must be in JAVA
  *
@@ -30,15 +37,25 @@ public class FindMissingNumbersIn1_NUnSorted {
 
     public static void main(String[] args) {
 
+        findDisappearedNumbersEx();
+        findDisappearedNumbersWithUpperElementEx();
+
+    }
+
+
+    private static void findDisappearedNumbersEx() {
+
         int[] nums;
 
         nums = new int[]{4, 3, 2, 7, 8, 2, 3, 1};
-        System.out.println("Missing numbers = " + findDisappearedNumbers(nums));
+        System.out.println("Missing numbers = " + findDisappearedNumbers(nums)); // [5, 6]
 
         nums = new int[]{1, 1};
-        System.out.println("Missing numbers = " + findDisappearedNumbers(nums));
+        System.out.println("Missing numbers = " + findDisappearedNumbers(nums)); // [2]
+
     }
 
+    // Constrain is here is that - to total no of element  = N ( max value of array)
     private static List<Integer> findDisappearedNumbers(int[] nums) {
 
         List<Integer> missingNums = new ArrayList<>();
@@ -57,4 +74,38 @@ public class FindMissingNumbersIn1_NUnSorted {
 
     }
 
+    private static void findDisappearedNumbersWithUpperElementEx() {
+
+        int[] nums = new int[]{1, 5, 3, 4, 7};
+        System.out.println("Missing numbers = " + findDisappearedNumbersWithUpperElement(nums)); // [2, 6]
+        System.out.println("Missing numbers = " + findDisappearedNumbersWithUpperElement_stream(nums)); // [2, 6]
+
+    }
+
+    /*
+     * Find All Numbers Disappeared in an Array:
+     * test:[1, 5, 3, 4, 7]
+     * ans: [2, 6]
+     * The solution must be in JAVA
+     *
+     *  duplicates are not here, so hashSet is not required
+    * */
+    private static List<Integer> findDisappearedNumbersWithUpperElement(int[] arr) {
+        List<Integer> missingNums = new ArrayList<>();
+
+        int max = ArrayUtils.findMax(arr);
+        for (int i = 1; i <= max; i++)
+            if (-1 == ArrayUtils.linearSearch(arr, i))
+                missingNums.add(i);
+
+        return missingNums;
+    }
+
+    private static List<Integer> findDisappearedNumbersWithUpperElement_stream(int[] arr) {
+        return IntStream.rangeClosed(1, ArrayUtils.findMax(arr))
+                        .filter(e -> -1 == ArrayUtils.linearSearch(arr,e))
+                        .boxed()
+                        .collect(Collectors.toList());
+
+    }
 }
