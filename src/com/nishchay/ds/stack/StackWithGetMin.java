89038fb2265d3ede7dedeadefe787271ed43ebf4
nnares <nishchay.naresh@gmail.com>
2022-01-31 00:26:27 +0530

    + ds.stack.StackWithGetMin.java

---
+++ src/com/nishchay/ds/stack/StackWithGetMin.java
@@ -0,0 +1,29 @@
+package com.nishchay.ds.stack;
+
+
+/*
+ *	=========== Design a stack that supports getMin() in O(1) time and O(1) extra space ===========
+ *
+ *	Design a Data Structure SpecialStack that supports all the stack operations like push(), pop(), isEmpty(), isFull()
+ *  and an additional operation getMin() which should return minimum element from the SpecialStack.
+ *  All these operations of SpecialStack must be O(1) time and O(1) extra space.
+ *
+ *
+ *	Example 1:
+ *		Input: nums = [4,3,2,7,8,2,3,1]
+ *		Output: [5,6]
+ *
+ *	Example 2:
+ *		Input: nums = [1,1]
+ *		Output: [2]
+ *
+ * https://www.youtube.com/watch?v=QMlDCR9xyd8
+ * https://www.geeksforgeeks.org/design-a-stack-that-supports-getmin-in-o1-time-and-o1-extra-space/
+ *
+ * */
+public class StackWithGetMin {
+
+    public static void main(String[] args) {
+
+    }
+}
