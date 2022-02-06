abe01fd96fbf85a698d3c84cdc7310d8f5c6402d
nnares <nishchay.naresh@gmail.com>
2022-02-06 08:52:18 +0530

    + u ds.array.nelement

--- src/com/nishchay/ds/array/nelement/FindManyRepetitiveElementIn1_N.java
+++ src/com/nishchay/ds/array/nelement/FindManyRepetitiveElementIn1_N.java
@@ -8,21 +8,15 @@ package com.nishchay.ds.array.nelement;
  * Given an array of n elements that contains elements from 0 to n-1, with any of these numbers appearing any number of times.
  * Find these repeating numbers in O(n) and using only constant memory space.
  *
- *	We are given a 2D array where all elements in any individual row or column are sorted.
- * 	In such a matrix, we have to search or find the position of, a given key.
  *
  *	For Example
  *		Input : n = 7 and array[] = {1, 2, 3, 6, 3, 6, 1}
  *		Output: 1, 3, 6
- *
- *		Explanation: The numbers 1 , 3 and 6 appears more
- *		than once in the array.
+ *		Explanation: The numbers 1 , 3 and 6 appears more than once in the array.
  *
  *		Input : n = 5 and array[] = {1, 2, 3, 4 ,3}
  *		Output: 3
- *
- *		Explanation: The number 3 appears more than once
- *		in the array.
+ *		Explanation: The number 3 appears more than once in the array.
  *
  *
  * https://www.geeksforgeeks.org/find-duplicates-in-on-time-and-constant-extra-space/
