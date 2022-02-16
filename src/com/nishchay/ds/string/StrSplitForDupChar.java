a35e912768bea169bc6ccef752fcd612dec2ed38
nnares <nishchay.naresh@gmail.com>
2022-02-16 13:12:02 +0530

    +u ds.string.StrSplitForDupChar.java

--- src/com/nishchay/ds/string/StrSplitForDupChar.java
+++ src/com/nishchay/ds/string/StrSplitForDupChar.java
@@ -37,19 +37,20 @@ public class StrSplitForDupChar {
 
     private static List<String> strSplitForDup(String input) {
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder builder = new StringBuilder();
         List<String> tuples = new ArrayList<>();
 
         for (int i = 0; i < input.length(); i++) {
             if ( i < input.length()-1 && input.charAt(i) == input.charAt(i+1)){
-                sb.append(input.charAt(i) + " ");
-                tuples.add(sb.toString());
-                sb.setLength(0);
+                builder.append(input.charAt(i)).append(" ");
+                tuples.add(builder.toString());
+                // flush builder previous content
+                builder.setLength(0);
             }else{
-                sb.append(input.charAt(i));
+                builder.append(input.charAt(i));
             }
         }
-        tuples.add(sb.toString());
+        tuples.add(builder.toString());
         return tuples;
     }
 
