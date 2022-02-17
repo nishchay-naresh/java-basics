3fa222229e04a6fd0dcfe13c2f901eb3d8818d3f
nnares <nishchay.naresh@gmail.com>
2022-02-17 09:31:59 +0530

    + ds.array.search.BS3OrderAgnosticSearch.java

---
+++ src/com/nishchay/ds/array/search/BS3OrderAgnosticSearch.java
@@ -0,0 +1,27 @@
+package com.nishchay.ds.array.search;
+/*
+ *	=========== Order-agnostic Binary Search ===========
+ *
+ *  Given a sorted array of numbers, find if a given number key is present in the array.
+ *  Though we know that the array is sorted, we don’t know if it’s sorted in ascending or descending order.
+ *
+ *
+ *	Example 1:
+ *		Input: [2, 8, 11, 19], key = 11
+ *		Output: 2
+ *
+ *	Example 2:
+ *		Input: [32, 28, 17, 9, 3], key = 9
+ *		Output: 3
+ *
+ *
+ * https://www.geeksforgeeks.org/order-agnostic-binary-search/
+ * https://www.callicoder.com/order-agnostic-binary-search/
+ *
+ * */
+public class BS3OrderAgnosticSearch {
+
+    public static void main(String[] args) {
+
+    }
+}
