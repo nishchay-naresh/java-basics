611898519568b3be7a501fa18c724db6d4d6114e
nnares <nishchay.naresh@gmail.com>
2022-02-20 12:46:07 +0530

    + ds.string.freq.WordSrchInLine.java

---
+++ src/com/nishchay/ds/string/freq/WordSrchInLine.java
@@ -0,0 +1,19 @@
+package com.nishchay.ds.string.freq;
+
+/*
+implement a word search in over multiple lines of text and return the matching line
+
+Str1 = "this is a test"
+Str2 = "testing for content indexing"
+Str3 = "testing for search text in test1"
+Strn N = "……"
+
+Search key = "test" (whole word search) returns str1
+* */
+
+public class WordSrchInLine {
+
+    public static void main(String[] args) {
+
+    }
+}
