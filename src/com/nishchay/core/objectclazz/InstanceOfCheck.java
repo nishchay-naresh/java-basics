a45e2bafb3e2315ae67cf1a4b9989879b4301db1
nnares <nishchay.naresh@gmail.com>
2022-02-23 10:49:25 +0530

    +u core.objectclazz.InstanceOfCheck.java

--- src/com/nishchay/core/objectclazz/InstanceOfCheck.java
+++ src/com/nishchay/core/objectclazz/InstanceOfCheck.java
@@ -1,9 +1,29 @@
 package com.nishchay.core.objectclazz;
 
-
+/*
+ *============== instanceof Operator ====================
+ *
+ * instanceof is a binary operator used to test if an object is of a given type. The result of the operation is either true or false.
+ *
+ * The instanceof operator works on the principle of the is-a relationship.
+ *
+ *
+ * https://www.baeldung.com/java-instanceof
+ */
 public class InstanceOfCheck {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
+
+        /*
+        * Using the instanceof Operator When an Object Is null
+        *
+        * If we use the instanceof operator on any object that is null, it returns false.
+        * Also, no null check is needed when using an instanceof operator.
+        * */
+        Parent ref = null;
+        System.out.println(ref instanceof Parent);
+
+
         Parent p = new Parent();
         Child c = new Child();
         System.out.println("child instanceof Parent " + (c instanceof Parent)); // true
