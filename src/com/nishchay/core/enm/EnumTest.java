d5c8a03f6943e224153109a5b7171f20c2c67102
nnares <nishchay.naresh@gmail.com>
2022-02-24 12:50:21 +0530

    +u core.enm.EnumTest.java

--- src/com/nishchay/core/enm/EnumTest.java
+++ src/com/nishchay/core/enm/EnumTest.java
@@ -4,16 +4,22 @@ public class EnumTest {
 
 
     public static void main(String[] args) {
-            Days d1 = Days.SUN;
+        Days d1 = Days.SUN;
         String d1Name = d1.name();
         Days d2 = Days.valueOf(d1Name);
 
-        String res = d1 == d2?"equal":"unequal";
+        String res = d1 == d2 ? "equal" : "unequal";
         System.out.println("d1 == d2 " + res);
 
-        for(Days d : Days.values()){
+        d1 = Days.SAT;
+        d2 = Days.MON;
+        // ordinal is like index
+        System.out.printf("ordinal : %d = %d%n", d1.ordinal(), d2.ordinal());
+
+        for (Days d : Days.values()) {
             System.out.println(d.name());
         }
+
     }
 
 }
