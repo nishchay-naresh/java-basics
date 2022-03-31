27bf76f4e3aadb259981e0a13db213e4563fdb21
nnares <nishchay.naresh@gmail.com>
2022-03-31 11:41:09 +0530

    +u ds.tree.CountNodes.java

--- src/com/nishchay/ds/tree/CountNodes.java
+++ src/com/nishchay/ds/tree/CountNodes.java
@@ -37,5 +37,22 @@ public class CountNodes {
             return getCountNode0(node.left) + getCountNode0(node.right);
     }
 
+    private static int getCountNode1(Node node) {
+        if (node == null)
+            return 0;
+        if (node.left == null || node.right == null)
+            return 1;
+        else
+            return getCountNode1(node.left) + getCountNode1(node.right);
+    }
+
+    private static int getCountNode2(Node node) {
+        if (node == null)
+            return 0;
+        if (node.left != null && node.right != null)
+            return 1;
+        else
+            return getCountNode2(node.left) + getCountNode2(node.right);
+    }
 
 }
