5368a50ad6dce7e8c624610842563b7bcedc422f
nnares <nishchay.naresh@gmail.com>
2022-05-08 09:23:22 +0530

    - core.spi

--- src/com/nishchay/core/spi/ConsoleGreetings.java
+++
@@ -1,8 +0,0 @@
-package com.nishchay.core.spi;
-
-public class ConsoleGreetings implements GreetingsService {
-    @Override
-    public void sayHello(String name) {
-        System.out.println( "Hello to " + name);
-    }
-}
--- src/com/nishchay/core/spi/ServiceLoaderDemo.java
+++
@@ -1,8 +0,0 @@
-package com.nishchay.core.spi;
-
-/*
-*
-* https://dzone.com/articles/play-with-java-serviceloader-forget-about-dependen
-* */
-public class ServiceLoaderDemo {
-}
