e4213f0999a94c1deedbf355e1fcb44fc270d562
nnares <nishchay.naresh@gmail.com>
2022-05-13 18:52:36 +0530

    +u nishchay.core.reflect.Driver.java

--- src/com/nishchay/core/reflect/Driver.java
+++ src/com/nishchay/core/reflect/Driver.java
@@ -1,33 +1,55 @@
 package com.nishchay.core.reflect;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
+/*
+ * Dynamic Proxy using Proxy and Invocation Handler
+ *
+ * Proxy - creating a proxy object using reflexion and exposing a hook of Invocation handler
+ * Invocation handler - intercepting all the method invocation, can add the extra things at run-time
+ * Wherever we have the Delegator use case we can use this.
+ *
+ * Its is very helpfully, when we have so many methods calls to delegate, without writing an implementation class for method call delegations only
+ *
+ * */
 public class Driver {
 
     public static void main(String[] argv) {
 
-        MyInterface ref = new MyInterfaceImpl();
-        ref.greet("Iron Man");
+        MyInterface impl = new MyInterfaceImpl();
+        impl.greet("Iron Man");
 
-        System.out.println("-----------intercepting method call using InvocationHandler----------");
-        MyInterface proxy = (MyInterface) Proxy.newProxyInstance(MyInterface.class.getClassLoader(),
-                new Class[] { MyInterface.class }, new MyInvocationHandler(ref));
-        proxy.greet("Wonder Women");
+        // intercepting method call using InvocationHandler
+        System.out.println("-----------InvocationHandler - Impl class ----------");
+        MyInterface dynamicProxy = (MyInterface) Proxy.newProxyInstance(MyInterface.class.getClassLoader(),
+                new Class[]{MyInterface.class}, new MyInvocationHandler(impl));
+        dynamicProxy.greet("Wonder Women");
 
-    }
-
-    public static MyInterface m1() {
-        final MyInterface ref = new MyInterfaceImpl();
-/*        return Proxy.newProxyInstance(MyInterface.class.getClassLoader(),
-                new Class[] { MyInterface.class }, (proxy, method, args) -> {
-                    Class<?>[] parameterTypes = method.getParameterTypes();
-                    for (int i =0; i < parameterTypes.length; i++) {
-                        if (String.class.equals(parameterTypes[i])) {
-                            args[i] = proxy + " :: " + args[i].toString();
-                        }
+        System.out.println("-----------InvocationHandler - Inner class ----------");
+        dynamicProxy = (MyInterface) Proxy.newProxyInstance(MyInterface.class.getClassLoader(),
+                new Class[]{MyInterface.class}, new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        System.out.println("Proxy using Inner class " + method.getName() + " - before execution");
+                        method.invoke(impl, args);
+                        System.out.println("Proxy using Inner class " + method.getName() + " - after execution");
+                        return null;
                     }
-                    return method.invoke(this, args);
-                });*/
-        return ref;
+                });
+        dynamicProxy.greet("Bat Man");
+
+        System.out.println("-----------InvocationHandler - Lambda ----------");
+        dynamicProxy = (MyInterface) Proxy.newProxyInstance(MyInterface.class.getClassLoader(),
+                new Class[]{MyInterface.class}, (proxy, method, args) -> {
+                    System.out.println("Proxy using lambda " + method.getName() + " - before execution");
+                    method.invoke(impl, args);
+                    System.out.println("Proxy using lambda " + method.getName() + " - after execution");
+                    return null;
+                });
+        dynamicProxy.greet("Captain America");
+
     }
 
 
