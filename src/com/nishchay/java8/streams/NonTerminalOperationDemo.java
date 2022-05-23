c77870123e00b1c8974e7cb95d11d1a0e84c2cd8
nnares <nishchay.naresh@gmail.com>
2022-05-23 10:10:06 +0530

    +u nishchay.java8.streams.NonTerminalOperationDemo.java

--- src/com/nishchay/java8/streams/NonTerminalOperationDemo.java
+++ src/com/nishchay/java8/streams/NonTerminalOperationDemo.java
@@ -1,5 +1,6 @@
 package com.nishchay.java8.streams;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -15,10 +16,27 @@ public class NonTerminalOperationDemo {
 //        filterDemo();
 //        distinctDemo();
 //        sortedDemo();
-        limitDemo();
-        skipDemo();
+//        limitDemo();
+//        skipDemo();
 //        peekDemo();
+        noStartsWith();
 
+    }
+
+    /*
+    * int arr[] = { 2, 1, 15, 10, 25, 101, 35, 19 }; (Start with 1 ) print
+    * output :: 1, 15, 10, 101, 19
+    * Do we have any Method like startWith(1) in Java 1.8 -> No
+    *
+    * */
+    private static void noStartsWith() {
+
+        int[] intArray = {2, 1, 15, 10, 25, 101, 35, 19};
+
+        List<Integer> integerList = Arrays.stream(intArray).boxed().collect(Collectors.toList());
+
+        List<String> result = integerList.stream().map(e -> e.toString()).filter(e -> e.startsWith("1")).collect(Collectors.toList());
+        System.out.println("result = " + result);
     }
 
     private static void createAndPrint(){
