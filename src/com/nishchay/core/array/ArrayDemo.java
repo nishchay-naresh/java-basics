501377e306d3f6c4e15821e788a23a64eb018ee3
nnares <nishchay.naresh@gmail.com>
2022-05-29 20:33:53 +0530

    +u nishchay.core.array.ArrayDemo.java

--- src/com/nishchay/core/array/ArrayDemo.java
+++ src/com/nishchay/core/array/ArrayDemo.java
@@ -9,8 +9,10 @@ public class ArrayDemo {
 
 		arrayEx();
         arrayCopyDemo();
+        compareArrayEx();
 
     }
+
 
     public static void arrayEx() {
         int[] intArray = new int[5];
@@ -31,11 +33,54 @@ public class ArrayDemo {
         int[] intArr = new int[]{5, 10, 15, 20, 25, 30};
         int[] copyArray = Arrays.copyOf(intArr, 4);
 
-        System.out.println("intArr = " + intArr);
-        System.out.println("copyArray = " + copyArray);
+        String copyStatus =  intArr==copyArray? "same copy" : "diff copy";
+        System.out.println("two new array is having  = " + copyStatus);
 
         System.out.println("intArr[] = " + Arrays.toString(intArr));
         System.out.println("copyArray[] = " + Arrays.toString(copyArray));
+
+        copyArray = Arrays.copyOf(intArr, intArr.length);
+        System.out.println("identical copy - " + Arrays.equals(intArr, copyArray));
     }
+
+    private static void compareArrayEx() {
+
+        primitiveArrCompare();
+        objectArrCompare();
+
+    }
+
+    private static void primitiveArrCompare() {
+
+        int[] intArray1 = new int[] { 1, 2, 3, 4, 5 };
+        int[] intArray2 = new int[] { 1, 2, 3, 4, 5 };
+
+        // Comparing arrays with Arrays.equals() method
+        boolean isSame = Arrays.equals(intArray1, intArray2);
+        System.out.println("\nArray comparison with Arrays.equals(intArray1, intArray2)");
+        if (isSame) {
+            System.out.println("int array 1 and array 2 are same");
+        } else {
+            System.out.println("int array 1 and array 2 are not same");
+        }
+    }
+
+    private static void objectArrCompare() {
+
+        String[] stringArr1 = {"Rohit", "Shikhar", "Kohli", "Iyyar", "Dhoni"};
+        String[] stringArr2 = {"Rohit", "Shikhar", "Kohli", "Iyyar", "Dhoni"};
+
+        // Comparing arrays with Arrays.equals() method
+        boolean isSame = Arrays.equals(stringArr1, stringArr2);
+        System.out.println("\nArray comparison with Arrays.equals(intArray1, intArray2)");
+        if (isSame) {
+            System.out.println("String array 1 and array 2 are same");
+        } else {
+            System.out.println("String array 1 and array 2 are not same");
+        }
+
+    }
+
+
 
 }
