863eafcf89c55a7bd99d4566441127b3d286b23f
Nishchay Naresh <nishchay.naresh@gmail.com>
2022-06-25 21:12:30 +0530

    + core.basics.overload.OverloadingDemo.java

--- src/com/nishchay/core/basics/overload/OverloadingDemo.java
+++ src/com/nishchay/core/basics/overload/OverloadingDemo.java
@@ -5,14 +5,39 @@ public class OverloadingDemo {
     public static void main(String[] args) {
 
         overloadBasedOnNonRelatedClass();
+        overloadBasedOnRelatedClass();
+
     }
+
 
     private static void overloadBasedOnNonRelatedClass(){
 
-        Test ref = new Test();
-//        ref.display("java");
-        ref.display(null);  // working bcus Object <- String
+        display("java");
+//        display(null);  // Ambiguous method call - not working bcus Thread, String are non-relatable class
 
+    }
+
+    private static void overloadBasedOnRelatedClass(){
+
+        m1("java");
+        m1(null);  // working bcus Object <- String
+
+    }
+
+    private static void display(String str) {
+        System.out.println("String");
+    }
+
+    private static void display(Thread t){
+        System.out.println("Thread");
+    }
+
+    private static void m1(Object obj) {
+        System.out.println("Object");
+    }
+
+    private static void m1(String str) {
+        System.out.println("String");
     }
 
 }
