a0910afab48cfe86819fad5256d1e388af9540d7
Nishchay Naresh <nishchay.naresh@gmail.com>
2022-06-30 12:58:42 +0530

    +u ds.array.nelement

--- src/com/nishchay/ds/array/nelement/FindManyRepetitiveElementIn1_N.java
+++ src/com/nishchay/ds/array/nelement/FindManyRepetitiveElementIn1_N.java
@@ -5,7 +5,7 @@ package com.nishchay.ds.array.nelement;
  *
  * Find duplicates in O(n) time and O(1) extra space.
  *
- * Given an array of n elements that contains elements from 0 to n-1, with any of these numbers appearing any number of times.
+ * Given an array of n elements that contains elements from 1 to n-1, with any of these numbers appearing any number of times.
  * Find these repeating numbers in O(n) and using only constant memory space.
  *
  *
@@ -28,13 +28,27 @@ public class FindManyRepetitiveElementIn1_N {
 
     public static void main(String[] args) {
 
-        int[] arr = {1, 2, 3, 1, 3, 6, 6};
+        int[] arr;
 
+        arr = new int[]{1, 2, 3, 4, 3};
         printRepeating(arr);
+
+        arr = new int[]{1, 2, 3, 1, 3, 6, 6};
+        printRepeating(arr);
+
+
     }
 
     /*
-     * Function to print duplicates in O(n) time
+     * In this array - each element could - 1 to N, we will use this properties
+     * traverse through each element, use it as index
+     *  if first time visit
+     *      put the -ve version of it
+     *  else
+     *      it's a duplicate
+     *
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
      *
      * Input: [2, 3, 3, 1]
      *   Index 0:
@@ -50,14 +64,14 @@ public class FindManyRepetitiveElementIn1_N {
      * */
     private static void printRepeating(int[] arr) {
         int i;
-        System.out.println("The repeating elements are : ");
+        System.out.print("\nThe repeating elements are : ");
 
         for (i = 0; i < arr.length; i++) {
-            int j = Math.abs(arr[i]);
-            if (arr[j] >= 0)
-                arr[j] = -arr[j];
+            int currElement = Math.abs(arr[i]);
+            if (arr[currElement] >= 0)
+                arr[currElement] = -arr[currElement];
             else
-                System.out.print(j + " ");
+                System.out.print(currElement + " ");
         }
     }
 
