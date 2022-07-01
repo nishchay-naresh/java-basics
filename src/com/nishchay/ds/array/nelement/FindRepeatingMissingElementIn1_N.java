996d7f765c500234baeb616f96973c52b87e516e
Nishchay <nishchay.naresh@gmail.com>
2022-07-01 11:00:18 +0530

    +u core.collection.MapDemo.java

--- src/com/nishchay/ds/array/nelement/FindRepeatingMissingElementIn1_N.java
+++ src/com/nishchay/ds/array/nelement/FindRepeatingMissingElementIn1_N.java
@@ -8,26 +8,31 @@ import java.util.Arrays;
  * One number from set {1, 2, …n} is missing and one number occurs twice in the array.
  * Find these two numbers.
  *
- *	Input: arr[] = {3, 1, 3}
+ *	Input: arr[] = {3, 1, 3}, n=3
  *	Output: Missing = 2, Repeating = 3
  *
- *	Input: arr[] = {4, 3, 6, 2, 1, 1}
+ *	Input: arr[] = {4, 3, 6, 2, 1, 1}, n=6
+ *	Output: Missing = 5, Repeating = 1
+ *
+ *  Input: arr[] = {7, 3, 4, 5, 5, 6, 2}, n=7
  *	Output: Missing = 5, Repeating = 1
  *
  * */
 class FindRepeatingMissingElementIn1_N {
 
     public static void main(String[] args) {
-        int[] arr = {7, 3, 4, 5, 5, 6, 2};
-        System.out.println("arr - " + Arrays.toString(arr));
+
+        int[] arr = {4, 3, 6, 2, 1, 1};
         findRepeatingMissing(arr);
+
     }
 
     private static void findRepeatingMissing(int[] arr) {
+        System.out.print("\nOriginal arr - " + Arrays.toString(arr));
         int size = arr.length;
         int[] tempArray = Arrays.copyOf(arr, size);
 
-        System.out.print("Repeating - ");
+        System.out.print("\nRepeating - ");
 
         for (int i = 0; i < size; i++) {
             int absCurrVal = Math.abs(tempArray[i]);
@@ -37,9 +42,9 @@ class FindRepeatingMissingElementIn1_N {
                 System.out.print(absCurrVal);
         }
 
-        System.out.println("arr - " + Arrays.toString(arr));
+        System.out.print("\nUpdated arr - " + Arrays.toString(tempArray));
 
-        System.out.print("\t Missing - ");
+        System.out.print("\nMissing - ");
         for (int i = 0; i < size; i++) {
             if (tempArray[i] > 0)
                 System.out.print(i + 1);
