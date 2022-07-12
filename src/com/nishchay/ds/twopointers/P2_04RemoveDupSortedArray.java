12678662826f908bea5d54b5ca28c3292413b146
Nishchay <nishchay.naresh@gmail.com>
2022-07-12 11:25:29 +0530

    +u ds.twopointers.P2_04RemoveDupSortedArray.java

--- src/com/nishchay/ds/twopointers/P2_04RemoveDupSortedArray.java
+++ src/com/nishchay/ds/twopointers/P2_04RemoveDupSortedArray.java
@@ -1,6 +1,7 @@
 package com.nishchay.ds.twopointers;
 
 import java.util.Arrays;
+
 /*
  *============== Remove duplicates from sorted array ====================
  *
@@ -35,7 +36,7 @@ public class P2_04RemoveDupSortedArray {
 
         extraSpaceWay();
         System.out.println("--------------------------------");
-//        twoPointersWay();
+        twoPointersWay();
 
     }
 
@@ -83,26 +84,87 @@ public class P2_04RemoveDupSortedArray {
         arr = new int[]{2, 2, 2, 2, 2};
         System.out.println("   with dups = " + Arrays.toString(arr));
         uniqueCount = removeDuplicates_copy(arr);
-        System.out.println("without dups = " + Arrays.toString(Arrays.copyOfRange(arr,0, uniqueCount)));
+        System.out.println("without dups = " + Arrays.toString(Arrays.copyOfRange(arr, 0, uniqueCount)));
         System.out.println(" uniqueCount = " + uniqueCount);
 
         arr = new int[]{1, 2, 2, 3, 4, 4, 4, 5, 5};
         System.out.println("   with dups = " + Arrays.toString(arr));
         uniqueCount = removeDuplicates_copy(arr);
-        System.out.println("without dups = " + Arrays.toString(Arrays.copyOfRange(arr,0, uniqueCount)));
+        System.out.println("without dups = " + Arrays.toString(Arrays.copyOfRange(arr, 0, uniqueCount)));
         System.out.println(" uniqueCount = " + uniqueCount);
 
         arr = new int[]{1, 1, 1, 3, 5, 5, 7};
         System.out.println("   with dups = " + Arrays.toString(arr));
         uniqueCount = removeDuplicates_copy(arr);
-        System.out.println("without dups = " + Arrays.toString(Arrays.copyOfRange(arr,0, uniqueCount)));
+        System.out.println("without dups = " + Arrays.toString(Arrays.copyOfRange(arr, 0, uniqueCount)));
         System.out.println(" uniqueCount = " + uniqueCount);
 
         arr = new int[]{};
         System.out.println("   with dups = " + Arrays.toString(arr));
         uniqueCount = removeDuplicates_copy(arr);
-        System.out.println("without dups = " + Arrays.toString(Arrays.copyOfRange(arr,0, uniqueCount)));
+        System.out.println("without dups = " + Arrays.toString(Arrays.copyOfRange(arr, 0, uniqueCount)));
         System.out.println(" uniqueCount = " + uniqueCount);
+    }
+
+    /*
+     * ========= Method 2 : Two pointer approach  =========
+     *
+     *	Just maintain a separate index for same array as maintained for different array in Method 1
+     *
+     * Time Complexity: O(n).
+     * Auxiliary Space: O(1).
+     *
+     * */
+    private static int removeDuplicates_twoPointers(int[] arr) {
+
+        int n = arr.length;
+
+        if (n == 0 || n == 1)
+            return n;
+
+        // unique element index
+        int j = 0;
+
+        // Doing same as done in Method 1, using the same index i.e. j for same array
+        for (int i = 0; i < n - 1; i++)
+            if (arr[i] != arr[i + 1])
+                arr[j++] = arr[i];
+
+        // Store the last element as whether it is unique or repeated, it hasn't stored previously
+        arr[j++] = arr[n - 1];
+
+        return j;
+    }
+
+    private static void twoPointersWay() {
+
+        int[] arr;
+        int uniqueCount;
+
+        arr = new int[]{2, 2, 2, 2, 2};
+        System.out.println("   with dups = " + Arrays.toString(arr));
+        uniqueCount = removeDuplicates_twoPointers(arr);
+        System.out.println("without dups = " + Arrays.toString(Arrays.copyOfRange(arr, 0, uniqueCount)));
+        System.out.println(" uniqueCount = " + uniqueCount);
+
+        arr = new int[]{1, 2, 2, 3, 4, 4, 4, 5, 5};
+        System.out.println("   with dups = " + Arrays.toString(arr));
+        uniqueCount = removeDuplicates_twoPointers(arr);
+        System.out.println("without dups = " + Arrays.toString(Arrays.copyOfRange(arr, 0, uniqueCount)));
+        System.out.println(" uniqueCount = " + uniqueCount);
+
+        arr = new int[]{1, 1, 1, 3, 5, 5, 7};
+        System.out.println("   with dups = " + Arrays.toString(arr));
+        uniqueCount = removeDuplicates_twoPointers(arr);
+        System.out.println("without dups = " + Arrays.toString(Arrays.copyOfRange(arr, 0, uniqueCount)));
+        System.out.println(" uniqueCount = " + uniqueCount);
+
+        arr = new int[]{};
+        System.out.println("   with dups = " + Arrays.toString(arr));
+        uniqueCount = removeDuplicates_twoPointers(arr);
+        System.out.println("without dups = " + Arrays.toString(Arrays.copyOfRange(arr, 0, uniqueCount)));
+        System.out.println(" uniqueCount = " + uniqueCount);
+
     }
 
 }
