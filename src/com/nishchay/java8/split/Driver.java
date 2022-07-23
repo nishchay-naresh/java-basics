c04934ee2a8b6307e0848885df4ef49f687dbd8c
Nishchay <nishchay.naresh@gmail.com>
2022-07-23 19:10:22 +0530

    + java8.split.Driver.java

---
+++ src/com/nishchay/java8/split/Driver.java
@@ -0,0 +1,25 @@
+package com.nishchay.java8.split;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+public class Driver {
+ 
+    public static void main(String[] args) {
+        List<Student> studentList = Arrays.asList(
+                new Student("Alex", 23),
+                new Student("Albert", 29),
+                new Student("Brad", 33),
+                new Student("Fred", 38),
+                new Student("Max", 43)
+        );
+  
+        StreamSupport.stream(new StudentSpliterator(studentList), true).forEach(e -> System.out.println("====>" + e));
+        System.out.println(StreamSupport.stream(new StudentSpliterator(studentList), true).mapToInt(Student::getAge).sum());
+  
+        StreamSupport.stream(new StudentSpliterator(studentList), false).forEach(e -> System.out.println("====>" + e));
+        System.out.println(StreamSupport.stream(new StudentSpliterator(studentList), false).mapToInt(Student::getAge).sum());
+    }
+
+}
