6ce0a16af2f12f5d3231934873f5daccff1c7429
Nishchay <nishchay.naresh@gmail.com>
2022-08-14 10:57:39 +0530

    +u core.collection.ArraysMethodDemo.java

--- src/com/nishchay/core/collection/ArraysMethodDemo.java
+++ src/com/nishchay/core/collection/ArraysMethodDemo.java
@@ -3,9 +3,18 @@ package com.nishchay.core.collection;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class ArraysSearch {
+public class ArraysMethodDemo {
 
     public static void main(String[] args) {
+
+//        sortNSearchEx();
+        fillEx();
+
+    }
+
+
+    private static void sortNSearchEx() {
+
         String[] cities = {"Bangalore", "Pune", "San Francisco", "New York"};
 
         Arrays.sort(cities, Comparator.naturalOrder());
@@ -16,6 +25,18 @@ public class ArraysSearch {
 
         int k = Arrays.binarySearch(cities, "xxxx");
         System.out.println("Found at = " + k); // -5
-
     }
+
+    private static void fillEx() {
+
+        final int len = 8;
+        char[] chars = new char[len];
+        Arrays.fill(chars, '$');
+        System.out.println("chars = " + Arrays.toString(chars));
+
+        int[] nums = new int[len];
+        Arrays.fill(nums, -1);
+        System.out.println("nums  = " + Arrays.toString(nums));
+    }
+
 }
