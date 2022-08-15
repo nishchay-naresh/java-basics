4c255dd2f49aad68e92b8e79425ec09849e00771
Nishchay <nishchay.naresh@gmail.com>
2022-08-15 17:01:05 +0530

    +u ds.matrix.MatrixTransposePrint.java

--- src/com/nishchay/ds/matrix/MatrixTransposePrint.java
+++ src/com/nishchay/ds/matrix/MatrixTransposePrint.java
@@ -2,6 +2,20 @@ package com.nishchay.ds.matrix;
 
 
 /*
+ *============== In-place convert matrix in specific order ====================
+ *
+ * Write a code to convert a matrix in specific way without using extra space
+ *
+ *   Input:
+ *          1 2 3
+ *          4 5 6
+ *          7 8 9
+ *
+ *   Output:
+ *          1 6 7
+ *          2 5 8
+ *          3 4 9
+ *
  *	----------original matrix-----------
  *
  *	2	5	6
@@ -21,7 +35,7 @@ package com.nishchay.ds.matrix;
  *	 c0,0	c1,0	c2,0
  *	 ]
  *
- *
+ * https://www.geeksforgeeks.org/in-place-convert-matrix-in-specific-order/
  * */
 
 public class MatrixTransposePrint {
