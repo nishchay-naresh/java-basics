02662a3d194623439ef999cd97b5eb1fafba7980
Nishchay <nishchay.naresh@gmail.com>
2022-10-15 10:07:27 +0530

    + java8.async.cf.CF07CompFutureNeverEnds.java

---
+++ src/com/nishchay/java8/async/cf/CF07CompFutureNeverEnds.java
@@ -0,0 +1,25 @@
+package com.nishchay.java8.async.cf;
+
+import static com.nishchay.java8.async.Utils.create;
+import static com.nishchay.java8.async.Utils.printIt;
+
+public class CF07CompFutureNeverEnds {
+
+    public static void main(String[] args) {
+
+        thenRunEx();
+
+    }
+
+    // we can keep adding the runnable task to a cf using thenRun() , its keep executing them
+    private static void thenRunEx() {
+
+        create(2)
+                .thenAccept(e -> printIt(e))
+                .thenRun(() -> System.out.println("all done"))
+                .thenRun(() -> System.out.println("not really"))
+                .thenRun(() -> System.out.println("keeps on going"))
+                .thenRun(() -> System.out.println("cf never ends"));
+    }
+
+}
