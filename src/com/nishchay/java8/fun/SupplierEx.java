6d1f1264b78a4faad22099819b68eedf543280eb
Nishchay <nishchay.naresh@gmail.com>
2022-10-20 10:32:21 +0530

    +u java8.fun.SupplierEx.java

--- src/com/nishchay/java8/fun/SupplierEx.java
+++ src/com/nishchay/java8/fun/SupplierEx.java
@@ -1,5 +1,8 @@
 package com.nishchay.java8.fun;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
@@ -7,6 +10,14 @@ public class SupplierEx {
 
     public static void main(String[] args) {
 
+        basicEx();
+        castSupplier();
+
+        suppFromAnotherSupplier();
+    }
+
+
+    private static void basicEx() {
         Supplier<String> strSupplier = () -> "dummy";
         System.out.println("strSupplier - " + strSupplier.get());
 
@@ -20,6 +31,35 @@ public class SupplierEx {
                 .orElseGet(strSupplier);
 
         System.out.println("result = " + result);
+    }
 
+    private static void castSupplier() {
+
+        Supplier<Map<?, ?>> genericMapSupplier = HashMap::new;
+        Supplier<Map<String, Integer>> mapSupplier =  castSupplier1(genericMapSupplier);
+        System.out.println("map = " + mapSupplier.get());
+
+    }
+
+    private static <L, M> Supplier<Map<L, M>> castSupplier1(Supplier<Map<?, ?>> map) {
+        return (Supplier) map;
+    }
+
+    // taking one supplier, creation another who does the work of FirstSupplier + something extra piece of code
+    private static void suppFromAnotherSupplier() {
+
+        Supplier<String> strSupplier = () -> "dummy";
+        System.out.println("strSupplier - " + strSupplier.get());
+
+        AtomicInteger x = new AtomicInteger(10);
+
+        Supplier<String> strSupplier1 = () -> {
+            System.out.println("nested");
+            x.set(100);
+            System.out.println("x = "+ x.get());
+            return strSupplier.get();
+        };
+
+        System.out.println("strSupplier1 - " + strSupplier1.get());
     }
 }
