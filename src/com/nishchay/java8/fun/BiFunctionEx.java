bd58c21325631603efb1ade399a323936ff0f89b
Nishchay <nishchay.naresh@gmail.com>
2022-11-01 11:10:39 +0530

    + java8.fun.BiFunctionEx.java

---
+++ src/com/nishchay/java8/fun/BiFunctionEx.java
@@ -0,0 +1,19 @@
+package com.nishchay.java8.fun;
+
+/*
+ *
+ * R apply(T t, U u)
+ *      – Applies this function to the given arguments and produces results.
+ * default BiFunction andThen(Function after)
+ *      – Returns a composed function that first applies this function to its input, and then applies the after function to the result.
+ *
+ *
+ * TODO - https://javabydeveloper.com/java-bifunction-guide-examples/
+ *
+ * */
+public class BiFunctionEx {
+
+    public static void main(String[] args) {
+
+    }
+}
