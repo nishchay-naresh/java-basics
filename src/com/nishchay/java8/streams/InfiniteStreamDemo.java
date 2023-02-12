d3d7fe82340c94ca4abb4a97b8ede3127b839bc7
Nishchay <nishchay.naresh@gmail.com>
2023-02-12 09:55:48 +0530

    +u java8.streams.InfiniteStreamDemo.java

--- src/com/nishchay/java8/streams/InfiniteStreamDemo.java
+++ src/com/nishchay/java8/streams/InfiniteStreamDemo.java
@@ -1,43 +1,66 @@
 package com.nishchay.java8.streams;
 
+import java.util.Random;
 import java.util.stream.Stream;
 
 public class InfiniteStreamDemo {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    //Given a number k, and a count n, find the total of double of n
-    //even numbers starting with k, where sqrt of each number is > 20
+        getUnboundedStream();
+        impVsDecEx();
 
-    int k = 121;
-    int n = 51;
-    System.out.println(compute_itr(k, n));
-    System.out.println(compute_stream(k, n));
-  }
+    }
 
-  public static int compute_stream(int k, int n) {
+    private static void getUnboundedStream() {
 
-    return Stream.iterate(k, e -> e + 1)          // unbounded, lazy
-                 .filter(e -> e % 2 == 0)        // unbounded, lazy
-                 .filter(e -> Math.sqrt(e) > 20) // unbounded, lazy
-                 .mapToInt(e -> e * 2)           //unbounded, lazy
-                 .limit(n)                      //sized, lazy
-                 .sum();
-  }
+        System.out.print("Stream.iterate() - ");
+        Stream.iterate(0, n -> n + 2)
+                .limit(10)
+                .forEach(e -> System.out.print(e + ", "));
 
-  public static int compute_itr(int k, int n) {
-     int result = 0;
 
-     int index = k;
-     int count = 0;
-     while(count < n) {
-       if(index % 2 == 0 && Math.sqrt(index) > 20) {
-         result += index * 2;
-         count++;
-       }
-       index++;
-     }
-     return result;
-  }
+        System.out.print("\nStream.generate() - ");
+        Stream.generate(() -> new Random().nextInt(1000))
+                .limit(10)
+                .forEach(e -> System.out.print(e + ", "));
+
+    }
+
+    private static void impVsDecEx() {
+        // Given a number k, and a count n, find the total of double of n
+        // even numbers starting with k, where sqrt of each number is > 20
+
+        int k = 121;
+        int n = 51;
+        System.out.println(computeIterative(k, n));
+        System.out.println(computeStream(k, n));
+
+    }
+
+    public static int computeStream(int k, int n) {
+
+        return Stream.iterate(k, e -> e + 1)            // unbounded, lazy
+                .filter(e -> e % 2 == 0)                // unbounded, lazy
+                .filter(e -> Math.sqrt(e) > 20)         // unbounded, lazy
+                .mapToInt(e -> e * 2)                   // unbounded, lazy
+                .limit(n)                               // sized, lazy
+                .sum();
+    }
+
+    public static int computeIterative(int k, int n) {
+        int result = 0;
+
+        int index = k;
+        int count = 0;
+        while (count < n) {
+            if (index % 2 == 0 && Math.sqrt(index) > 20) {
+                result += index * 2;
+                count++;
+            }
+            index++;
+        }
+        return result;
+    }
 
 }
