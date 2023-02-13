57418a6b5c7191fef1f2525277664faa52b890fc
Nishchay <nishchay.naresh@gmail.com>
2023-02-13 08:35:42 +0530

    +u java8.fun.BiFunctionEx.java

--- src/com/nishchay/java8/fun/BiFunctionEx.java
+++ src/com/nishchay/java8/fun/BiFunctionEx.java
@@ -1,19 +1,43 @@
 package com.nishchay.java8.fun;
 
+import java.util.function.BiFunction;
+
 /*
- *
+ *  BiFunction<T, U, R> f = t + u = r
  * R apply(T t, U u)
  *      – Applies this function to the given arguments and produces results.
  * default BiFunction andThen(Function after)
  *      – Returns a composed function that first applies this function to its input, and then applies the after function to the result.
  *
  *
- * TODO - https://javabydeveloper.com/java-bifunction-guide-examples/
- *
  * */
 public class BiFunctionEx {
 
     public static void main(String[] args) {
 
+        biFunctionBasicEx();
+
+//        hashMapComputeWithBiFunctionEx();
+//        hashMapMergeReplaceAllEx();
+//        hashMapPrintEx();
+
     }
+
+
+
+
+    private static void biFunctionBasicEx() {
+
+        // a basic apply() example
+        BiFunction<Integer, Integer, Integer> funAdd = (a, b) -> a + b;
+        System.out.println(funAdd.apply(10, 20)); // 30
+
+        BiFunction<Integer, Integer, Integer> funMult = (a, b) -> a * b;
+        System.out.println(funMult.apply(4, 9)); // 36
+
+        BiFunction<String, String, Integer> funAdd1 = (s1, s2) -> s1.length() + s2.length();
+        System.out.println(funAdd1.apply("java", "8")); // 5
+
+    }
+
 }
