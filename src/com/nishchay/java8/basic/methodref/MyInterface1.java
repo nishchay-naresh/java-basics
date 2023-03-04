863d75cb661f4667e572b096c9713ee29d0728d9
Nishchay <nishchay.naresh@gmail.com>
2023-03-04 10:37:42 +0530

    +u java8.basic.MyInterface1.java

--- src/com/nishchay/java8/basic/methodref/MyInterface1.java
+++ src/com/nishchay/java8/basic/methodref/MyInterface1.java
@@ -1,5 +1,10 @@
 package com.nishchay.java8.basic.methodref;
 
+
+/*
+* Any interface with a SAM(Single Abstract Method) is a functional interface
+*
+* */
 @FunctionalInterface
 interface MyInterface1 {
     void display();
