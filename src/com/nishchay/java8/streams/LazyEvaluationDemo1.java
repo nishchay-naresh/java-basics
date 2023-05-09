c9d035d46b9e384f422e96ab60764f988663423b
Nishchay <nishchay.naresh@gmail.com>
2023-05-09 12:38:50 +0530

    +u java8.streams.LazyEvaluationDemo1.java

--- src/com/nishchay/java8/streams/LazyEvaluationDemo1.java
+++ src/com/nishchay/java8/streams/LazyEvaluationDemo1.java
@@ -2,14 +2,19 @@ package com.nishchay.java8.streams;
 
 import com.nishchay.util.Utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
 public class LazyEvaluationDemo1 {
     public static void main(String[] args) {
 
         eagerEvaluation_logicalOp1(evaluate(1),evaluate(2));
-//        System.out.println("-------------------------------");
-//        lazyEvaluationUsingSupplier1(() -> evaluate(1), () -> evaluate(2));
-//
-//        eagerEvaluation_collection1();
+        System.out.println("-------------------------------");
+        lazyEvaluationUsingSupplier1(() -> evaluate(1), () -> evaluate(2));
+
+        eagerEvaluation_collection1();
 //        lazyEvaluationInStream1();
     }
 
@@ -22,6 +27,35 @@ public class LazyEvaluationDemo1 {
     public static void eagerEvaluation_logicalOp1(final boolean input1, final boolean input2) {
         System.out.println("eagerEvaluator called...");
         System.out.println("accept?: " + (input1 && input2));
+    }
+
+    public static void lazyEvaluationUsingSupplier1(final Supplier<Boolean> input1, final Supplier<Boolean> input2) {
+        System.out.println("lazyEvaluator called...");
+        System.out.println("accept?: " + (input1.get() && input2.get()));
+    }
+
+    private static void eagerEvaluation_collection1() {
+
+        List<String> names = Arrays.asList("Brad", "Kate", "Kim", "Jack", "Joe",
+                "Mike", "Susan", "George", "Robert", "Julia", "Parker", "Benson");
+
+        List<String> result = new ArrayList<>();
+        for(String curr : names) {
+            if(length(curr)  == 3) {
+                result.add(toUpper(curr));
+            }
+        }
+        System.out.println(result.get(0));
+    }
+
+    private static int length(final String name) {
+        System.out.println("getting length for " + name);
+        return name.length();
+    }
+
+    private static String toUpper(final String name ) {
+        System.out.println("converting to uppercase: " + name);
+        return name.toUpperCase();
     }
 
 }
