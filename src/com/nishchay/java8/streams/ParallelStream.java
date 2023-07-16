ede5a27ad272b17d8cf6b7bf5912b194733dae5b
Nishchay <nishchay.naresh@gmail.com>
2023-07-16 12:00:48 +0530

    +u java8.streams.ParallelStream.java

--- src/com/nishchay/java8/streams/ParallelStream.java
+++ src/com/nishchay/java8/streams/ParallelStream.java
@@ -19,33 +19,32 @@ import static com.nishchay.util.Utils.timeIt;
 * mutability and parallel don't go together, you can’t mix them
 * sometimes we end of getting wrong result bcus of this mutability
 *
-*  How to turn the code in parallelStream, when you are not creating the stream, instead you are receiving stream
-*
 *
 * Streams			:	sequential vs parallel
 * Reactive Stream	:	sync vs. async
+*
 * */
 public class ParallelStream {
 
     public static void main(String[] args) {
 
         final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-//        serialStreamEvaluation(numbers);
-//        parallelStreamEvaluation(numbers);
+        serialStreamEvaluation(numbers);
+        parallelStreamEvaluation(numbers);
 
         receivedStreamInParallelExc(numbers);
 
+        threadsFormula();
     }
 
 
 
     // Martin Fowler : Collection pipeline pattern
     private static void serialStreamEvaluation(List<Integer> numbers) {
-
         timeIt(() ->
                 numbers.stream()
                 .filter(e -> e % 2 == 0)
-                .mapToInt(ParallelStream::compute)
+                .mapToInt(ParallelStream::doubledIt)
                 .sum()
         );
     }
@@ -56,13 +55,13 @@ public class ParallelStream {
         timeIt(() ->
                 numbers.parallelStream()
                         .filter(e -> e % 2 == 0)
-                        .mapToInt(ParallelStream::compute)
+                        .mapToInt(ParallelStream::doubledIt)
                         .sum()
         );
 
     }
 
-    private static int compute(int number) {
+    private static int doubledIt(int number) {
         // assume this is time intensive
         delay(1);
         return number * 2;
@@ -73,6 +72,9 @@ public class ParallelStream {
     }
 
     /*
+    *
+    *  How to turn the code in parallelStream, when you are not creating the stream, instead you are receiving stream
+    *
     *  Evaluation will go which comes first from bottom to top from terminal operation
     *  after the fist switch (parallel/sequential) later switches are getting ignored
     *  entire pipeline is sequential or parallel no segments
@@ -81,12 +83,52 @@ public class ParallelStream {
     private static void useIt(Stream<Integer> stream) {
         stream
             .parallel() // no op because of sequential operation
-            .mapToInt(ParallelStream::compute)
+            .mapToInt(ParallelStream::doubledIt)
             .sequential()
             .forEach(System.out::println);
     }
 
 
+    /*
+     * ================ How many threads ===============
+     *
+     * How many threads can I create? //bad question
+     * How much food can I eat?
+     *
+     * How many threads should I create?
+     * How much food should I eat?
+     *
+     *
+     * Two kind of operation we can perform :
+     * Computation intensive. <- Never give more threads than number of cores. Threads <= No of cores
+     * IO intensive. <- May be more than number of cores, but again how many don't know
+     *
+     * Blocking Factor - Ratio of time the task is waiting on IO
+     *
+     * Blocking factor for computation intensive = 0
+     * Blocking factor 0 <= Blocking Factor < 1
+     *
+     *                             Core
+     * Number of threads =  _____________________
+     *                       1 - Blocking Factor
+     *
+     *
+     * */
+    public static void threadsFormula() {
+
+        // System.out.println(Runtime.getRuntime().availableProcessors()); // No of cores
+        // System.out.println(ForkJoinPool.commonPool()); // parallelism = No of cores - 1(main)
+
+        List<Integer> numbers = Arrays.asList(
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30);
+
+        numbers
+                .parallelStream()
+                .map(ObservingAThread::compute)
+                .forEach(e -> {});
+    }
 }
 
 /*
