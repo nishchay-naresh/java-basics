871bdb350c3493752a6236d2726a7fffa3a949a0
Nishchay <nishchay.naresh@gmail.com>
2023-07-22 20:15:50 +0530

    +u java8.async.cf.CF02CompleteAFuture.java

--- src/com/nishchay/java8/async/cf/CF02CompleteAFuture.java
+++ src/com/nishchay/java8/async/cf/CF02CompleteAFuture.java
@@ -4,34 +4,75 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+import static com.nishchay.util.Utils.delay;
+
 public class CF02CompleteAFuture {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         completingAFuture();
+        lazyPipelineEx();
+
         completedFutureEx();
         completingAFutureExceptionally();
 
     }
 
 
-
+    /*
+     *
+     * CF.get() -    It's a blocking call
+     *               Waits if necessary for this future to complete, and then returns its result.
+     *
+     * CF.join() -   It's a blocking call
+     *               Waits if necessary for this future to complete, and then returns its result,
+     *               Throws an (unchecked) exception if completed exceptionally
+     *
+     * Until someone is not completing the CF, CF.get()/CF.join() call will be blocking
+     *
+     * One can complete a CF by calling complete(value) method
+     *
+     * */
     private static void completingAFuture() throws ExecutionException, InterruptedException {
-
         CompletableFuture<String> cf = new CompletableFuture<>();
+
         // completing the cf by calling complete() method
-        cf.complete("dummy value");
+        new Thread(() -> {
+            delay(2);
+            cf.complete("dummy value");
+        }).start();
 
-        // again this.get() call will get blocked, we need to complete this cf
-        String result = cf.get();
-        System.out.println("cf.get() = " + cf.get());
-
-
-        CompletableFuture<Integer> cf1 = new CompletableFuture<>();
-        cf1.complete(999);
-        System.out.println("cf.join() = " + cf1.join());
-
+        // cf.get() call will get blocked, we need to complete this cf
+        // System.out.println("cf.get() = " + cf.get());
+        System.out.println("cf.join() = " + cf.join());
     }
+
+
+    /*
+     * ================ creating a pipe, then completing =================
+     *
+     * Create a pipeline first then push the data through the pipeline - evaluating lazily
+     * We build the pipeline but no data is through this pipeline - so no output, irrespective how long we wait
+     *
+     * We can pass the data to this pipeline by calling/completing a CF -  future.complete(data)
+     *
+     * */
+    private static void lazyPipelineEx() {
+        CompletableFuture<Integer> future = new CompletableFuture<>();
+        future
+                .thenApply(data -> data * 2)
+                .thenApply(data -> data + 1)
+                .thenAccept(data -> System.out.println(data));
+
+        System.out.println("build the pipeline");
+        delay(1);
+
+        future.complete(2);
+
+        delay(1);
+        System.out.println("prepared to print");
+    }
+
 
     /*
      * completedFuture(v) - Returns a new CompletableFuture that is already completed with
