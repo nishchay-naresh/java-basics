ea367db5b88121fa97b73a2c9a9dd08cd755043b
Nishchay <nishchay.naresh@gmail.com>
2023-08-25 09:39:33 +0530

    + java8.collection.map.InitializeAHashMap.java

---
+++ src/com/nishchay/java8/collection/map/InitializeAHashMap.java
@@ -0,0 +1,14 @@
+package com.nishchay.java8.collection.map;
+
+
+/*
+*
+* https://www.baeldung.com/java-initialize-hashmap
+* */
+public class InitializeAHashMap {
+    public static void main(String[] args) {
+
+    }
+
+
+}
