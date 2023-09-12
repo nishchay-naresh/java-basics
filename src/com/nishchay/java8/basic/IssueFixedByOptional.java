3314c6a5b992794b9139e07499379cfc1043e913
Nishchay <nishchay.naresh@gmail.com>
2023-09-12 13:52:55 +0530

    + java8.basic.IssueFixedByOptional.java

---
+++ src/com/nishchay/java8/basic/IssueFixedByOptional.java
@@ -0,0 +1,55 @@
+package com.nishchay.java8.basic;
+
+import java.util.Optional;
+
+public class IssueFixedByOptional {
+
+    public String getCarInsuranceName(Person person) {
+        Optional<Person> optPerson = Optional.of(person);
+        return optPerson.flatMap(Person::getCar)
+                .flatMap(Car::getInsurance)
+                .map(Insurance::getName)
+                .orElse("unknown");
+    }
+
+    public String getCarInsuranceName_filter(Optional<Person> optPerson, int minAge) {
+        return optPerson.filter(p -> p.getAge() >= minAge)
+                .flatMap(Person::getCar)
+                .flatMap(Car::getInsurance)
+                .map(Insurance::getName)
+                .orElse("unknown");
+    }
+
+
+    class Person {
+        private Optional<Car> car;
+        private int age;
+
+        public Optional<Car> getCar() {
+            return car;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+    }
+
+    class Car {
+
+        private Optional<Insurance> insurance;
+
+        public Optional<Insurance> getInsurance() {
+            return insurance;
+        }
+    }
+
+    class Insurance {
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+    }
+}
