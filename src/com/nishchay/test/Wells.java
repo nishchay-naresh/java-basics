b01ba938cf5a925700a58e298a740be888ac2639
Nishchay <nishchay.naresh@gmail.com>
2023-09-22 11:33:24 +0530

    + nishchay.test.Wells.java

--- src/com/nishchay/test/Wells.java
+++ src/com/nishchay/test/Wells.java
@@ -17,7 +17,7 @@ import java.util.List;
  *	('a' and 'b' denote a house with a filter of a given type installed ;'?'represents a with house is no filter yet).
  *	Your task is to make a plan of the filter types to be installed in the houses that do not yet have them.
  *
- *	Wrtite a function :
+ *	Write a function :
  *	class Solution {
  *		public String[] solution (String s);
  *	}
@@ -28,7 +28,7 @@ import java.util.List;
  *	Examples:
  *	I. Given s="a?bb" your function should return "aabb"
  *	2. Given s="??abb" your function should return "ababb","bbabb: or "baabb"
- *	3. Given s="a?b??aa" your function should return "aababaa" // aab??aa,abb??aa // aab??aa -aababaa //abb??aa-abbabaa,
+ *	3. Given s="a?bba??aa" your function should return "aabbaabaa" or "aabbabbaa"
  *	4. Given s="aa??aa" your function should return "aabbaa"
  *	Write a efficient algorithm for the following
  *	assumptions:
@@ -37,15 +37,15 @@ import java.util.List;
  *	N is an integer within the range [I ..500,0001;
  *	It is always possible to create a plan so that there are no three identical consecutive filters.
  *
- * TODO - Case 3 is bit tricky one, Its combine both of the solved cases
- *        Need to give a thought on this
+ * Case 5. When ur string have both the ? sequence('?','??') , and the distance between then is less than 3
+ * Given s="a?b??aa" your function should return "aababaa" // aab??aa,abb??aa // aab??aa -aababaa //abb??aa-abbabaa,
  *
  * */
 public class Wells {
 
     public static void main(String[] args) {
 
-        String[] inputs = new String[]{"a?bb", "??abb", "aa??aa"};
+        String[] inputs = new String[]{"a?bb", "??abb", "aa??aa", "a?bba??aa"};
 
         for (String input : inputs) {
             List<String> possibleStrings = solution(input);
@@ -56,6 +56,14 @@ public class Wells {
     }
 
     public static List<String> solution(String str) {
+        if (str.contains("??") && str.contains("?")){
+            List<String> inputStrings = substituteDoubleQ(str);
+            List<String> possibleStrings = new ArrayList<>();
+            for (String input : inputStrings) {
+                possibleStrings.addAll(substituteSingleQ(input));
+            }
+            return possibleStrings;
+        }
         return str.contains("??") ? substituteDoubleQ(str) : substituteSingleQ(str);
     }
 
